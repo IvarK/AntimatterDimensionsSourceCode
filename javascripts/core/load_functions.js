5ef3feba19fe1d3deb59886ec49f858e1a04c82a
Omsi <omsi2000@gmail.com>
2018-08-28 12:04:16 -0700

    make autoeternitymode stay through resets

--- javascripts/core/load_functions.js
+++ javascripts/core/load_functions.js
@@ -127,7 +127,7 @@ function onLoad() {
   if (player.lastTenRealities === undefined) player.lastTenRealities = [[60000*60*24*31, new Decimal(1), 0], [60000*60*24*31, new Decimal(1), 0], [60000*60*24*31, new Decimal(1), 0], [60000*60*24*31, new Decimal(1), 0], [60000*60*24*31, new Decimal(1), 0], [60000*60*24*31, new Decimal(1), 0], [60000*60*24*31, new Decimal(1), 0], [60000*60*24*31, new Decimal(1), 0], [60000*60*24*31, new Decimal(1), 0], [60000*60*24*31, new Decimal(1), 0]];
   if (player.wormhole === undefined) player.wormhole = { speed: 60 * 60, power: 5, duration: 10, phase: 0, active: false, unlocked: false }
   if (player.reality === undefined) player.reality = { realityMachines: new Decimal(0), glyphs: {active: [], inventory: [], slots: 3, last: ""}, seed: Math.floor(Date.now() * Math.random()+1), upg: [], upgReqs: [null, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false], upgReqChecks: [false], automatorRows: 0, automatorCommands: [], respec: false };
-  if (player.autoEternityMode === undefined) player.autoEternityMode = "amount"
+  if (player.autoEternityMode === undefined) player.autoEternityMode = "amount";
   setTheme(player.options.theme);
 
   sliderText.textContent = "Update rate: " + player.options.updateRate + "ms";

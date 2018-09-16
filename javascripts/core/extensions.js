d9590f1e49105019f31f476c2bfa6fc5b37db71b
Andrei Andreev <Razenpok@outlook.com>
2018-09-16 12:38:12 +0200

    Extract whole theme functionality into separate module

--- javascripts/core/extensions.js
+++ javascripts/core/extensions.js
@@ -2,4 +2,10 @@ Array.prototype.distinct = function() {
     return this.filter(function (value, index, self) {
         return self.indexOf(value) === index;
     });
-}
+};
+
+Math.wrap = function (number, min, max) {
+    let range = max - min + 1;
+    number = ((number - min) % range);
+    return number < 0 ? min + 1 + number : min + number;
+};

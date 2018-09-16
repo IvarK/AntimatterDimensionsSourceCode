4907bc85bbac2fd3b24d2e41d47e0b643429df16
Andrei Andreev <Razenpok@outlook.com>
2018-09-16 13:44:10 +0200

    Move notation options button handler to options.js for further refactoring

--- javascripts/core/app/options.js
+++ javascripts/core/app/options.js
@@ -5,3 +5,48 @@ document.getElementById("theme").onclick = function (ev) {
     let theme = themes[next];
     theme.set();
 };
+
+document.getElementById("notation").onclick = function () {
+    player.options.scientific = !player.options.scientific;
+    if (player.options.notation === "Infinity") {
+        player.options.notation = "Scientific";
+        document.getElementById("notation").textContent = ("Notation: Scientific")
+    } else if (player.options.notation === "Scientific") {
+        player.options.notation = "Engineering";
+        document.getElementById("notation").textContent = ("Notation: Engineering")
+    } else if (player.options.notation === "Engineering") {
+        player.options.notation = "Letters";
+        document.getElementById("notation").textContent = ("Notation: Letters")
+    } else if (player.options.notation === "Letters") {
+        player.options.notation = "Standard";
+        document.getElementById("notation").textContent = ("Notation: Standard")
+    } else if (player.options.notation === "Standard") {
+        player.options.notation = "Cancer";
+        document.getElementById("notation").textContent = ("Notation: Cancer")
+    } else if (player.options.notation === "Cancer") {
+        player.options.notation = "Mixed scientific";
+        document.getElementById("notation").textContent = ("Notation: Mixed scientific")
+    } else if (player.options.notation === "Mixed scientific") {
+        player.options.notation = "Mixed engineering";
+        document.getElementById("notation").textContent = ("Notation: Mixed engineering")
+    } else if (player.options.notation === "Mixed engineering") {
+        player.options.notation = "Logarithm";
+        document.getElementById("notation").textContent = ("Notation: Logarithm")
+    } else if (player.options.notation === "Logarithm") {
+        player.options.notation = "Brackets";
+        document.getElementById("notation").textContent = ("Notation: Brackets")
+    } else if (player.options.notation === "Brackets") {
+        player.options.notation = "Infinity";
+        document.getElementById("notation").textContent = ("Notation: Infinity")
+    }
+
+    updateLastTenRuns();
+    updateLastTenEternities();
+    updateLastTenRealities();
+    updateTickSpeed();
+    setAchieveTooltip();
+    updateCosts();
+    updateDilationUpgradeCosts();
+    updateAutobuyers();
+    document.getElementById("epmult").innerHTML = "You gain 5 times more EP<p>Currently: "+shortenDimensions(player.epmult)+"x<p>Cost: "+shortenDimensions(player.epmultCost)+" EP"
+};
--- javascripts/game.js
+++ javascripts/game.js
@@ -1722,54 +1722,6 @@ function percentToNextGlyphLevel() {
     return Math.min(((ret - Math.floor(ret)-retOffset) * 100), 99.9).toFixed(1)
 }
 
-
-
-document.getElementById("notation").onclick = function () {
-    player.options.scientific = !player.options.scientific;
-    if (player.options.notation === "Infinity") {
-        player.options.notation = "Scientific";
-        document.getElementById("notation").textContent = ("Notation: Scientific")
-    } else if (player.options.notation === "Scientific") {
-        player.options.notation = "Engineering";
-        document.getElementById("notation").textContent = ("Notation: Engineering")
-    } else if (player.options.notation === "Engineering") {
-        player.options.notation = "Letters";
-        document.getElementById("notation").textContent = ("Notation: Letters")
-    } else if (player.options.notation === "Letters") {
-        player.options.notation = "Standard";
-        document.getElementById("notation").textContent = ("Notation: Standard")
-    } else if (player.options.notation === "Standard") {
-        player.options.notation = "Cancer";
-        document.getElementById("notation").textContent = ("Notation: Cancer")
-    } else if (player.options.notation === "Cancer") {
-        player.options.notation = "Mixed scientific";
-        document.getElementById("notation").textContent = ("Notation: Mixed scientific")
-    } else if (player.options.notation === "Mixed scientific") {
-        player.options.notation = "Mixed engineering";
-        document.getElementById("notation").textContent = ("Notation: Mixed engineering")
-    } else if (player.options.notation === "Mixed engineering") {
-        player.options.notation = "Logarithm";
-        document.getElementById("notation").textContent = ("Notation: Logarithm")
-    } else if (player.options.notation === "Logarithm") {
-        player.options.notation = "Brackets";
-        document.getElementById("notation").textContent = ("Notation: Brackets")
-    } else if (player.options.notation === "Brackets") {
-      player.options.notation = "Infinity";
-      document.getElementById("notation").textContent = ("Notation: Infinity")
-    }
-
-    updateLastTenRuns();
-    updateLastTenEternities();
-    updateLastTenRealities();
-    updateTickSpeed();
-    setAchieveTooltip();
-    updateCosts();
-    updateDilationUpgradeCosts();
-    updateAutobuyers();
-    document.getElementById("epmult").innerHTML = "You gain 5 times more EP<p>Currently: "+shortenDimensions(player.epmult)+"x<p>Cost: "+shortenDimensions(player.epmultCost)+" EP"
-};
-
-
 document.getElementById("newsbtn").onclick = function() {
   if (!player.options.newsHidden) {
     document.getElementById("game").style.display = "none";

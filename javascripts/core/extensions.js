7e8caafe9a7fb019313cbe3619954968a9c2f7d5
Andrei Andreev <Razenpok@outlook.com>
2018-09-17 15:35:14 +0200

    Purge exportOutput hack, migrate to copyToClipboard()

--- javascripts/core/extensions.js
+++ javascripts/core/extensions.js
@@ -4,8 +4,29 @@ Array.prototype.distinct = function() {
     });
 };
 
-Math.wrap = function (number, min, max) {
+Math.wrap = function(number, min, max) {
     let range = max - min + 1;
     number = ((number - min) % range);
     return number < 0 ? min + 1 + number : min + number;
 };
+
+function copyToClipboard(str) {
+    try {
+        let el = document.createElement('textarea');
+        el.value = str;
+        document.body.appendChild(el);
+        el.select();
+        let result = document.execCommand('copy');
+        document.body.removeChild(el);
+        return result;
+    } catch(ex) {
+        console.log(ex);
+        return false;
+    }
+}
+
+function copyToClipboardAndNotify(str) {
+    if (copyToClipboard(str)) {
+        $.notify("Exported to clipboard", "info");
+    }
+}

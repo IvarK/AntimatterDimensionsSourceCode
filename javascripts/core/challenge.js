c4797080993f23f951202061b9b7540f6fb9556c
Andrei Andreev <Razenpok@outlook.com>
2018-09-19 20:21:52 +0200

    Extract second soft reset

--- javascripts/core/challenge.js
+++ javascripts/core/challenge.js
@@ -3,47 +3,16 @@ function startChallenge(name, target) {
         return;
     }
     player.currentChallenge = name;
-    player.thisInfinityTime = 0;
-    player.resets = 0;
-    player.galaxies = 0;
-    player.tickDecrease = 0.9;
     player.challengeTarget = target;
-    resetChallengeStuff();
-    resetDimensions();
-    applyChallengeModifiers();
-    skipResetsIfPossible();
-    if (player.replicanti.unl) player.replicanti.amount = new Decimal(1);
-    player.replicanti.galaxies = 0;
-    setInitialDimensionPower();
-    IPminpeak = new Decimal(0);
-    hidePreMilestone30Elements();
-    updateChallenges();
-    showTab("dimensions");
-    document.getElementById("replicantireset").innerHTML =
-        "Reset replicanti amount, but get a free galaxy<br>" + player.replicanti.galaxies + " replicated galaxies created.";
-    resetInfDimensions();
-    resetTickspeed();
-    updateTickSpeed();
-    resetMoney();
-    if (player.currentChallenge.includes("post") && player.currentEternityChall !== "")
-        giveAchievement("I wish I had gotten 7 eternities");
-    Marathon2 = 0;
-    updateChallengeElements();
-}
-
-function updateChallengeElements() {
+    secondSoftReset();
     document.getElementById("fifthRow").style.display = "none";
     document.getElementById("sixthRow").style.display = "none";
     document.getElementById("seventhRow").style.display = "none";
     document.getElementById("eightRow").style.display = "none";
-    if (name === "challenge12" || player.currentChallenge === "postc1" || player.currentChallenge === "postc6")
-        document.getElementById("matter").style.display = "block";
-    else
-        document.getElementById("matter").style.display = "none";
-    if (name === "challenge12" || name === "challenge9" || name === "challenge5" || player.currentChallenge === "postc1" || player.currentChallenge === "postc4" || player.currentChallenge === "postc5" || player.currentChallenge === "postc6" || player.currentChallenge === "postc8")
-        document.getElementById("quickReset").style.display = "inline-block";
-    else
-        document.getElementById("quickReset").style.display = "none";
+    showTab("dimensions");
+    if (player.currentChallenge.includes("post") && player.currentEternityChall !== "")
+        giveAchievement("I wish I had gotten 7 eternities");
+    Marathon2 = 0;
 }
 
 function askChallengeConfirmation(challenge) {

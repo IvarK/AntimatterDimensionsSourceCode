0eb6250b195fd1822d917450d340546de439f2ab
IvarK <ivarkera@gmail.com>
2018-09-28 10:53:50 +0300

    trying to fix galaxy issue

--- javascripts/core/tickspeed.js
+++ javascripts/core/tickspeed.js
@@ -5,17 +5,17 @@ function canBuyTickSpeed() {
 
 function getTickSpeedMultiplier() {
   if (player.currentChallenge == "postc3") return 1;
-  if (player.galaxies + player.replicanti.galaxies + player.dilation.freeGalaxies < 3) {
+  let galaxies = player.galaxies+player.replicanti.galaxies+player.dilation.freeGalaxies
+  if (player.timestudy.studies.includes(133)) galaxies += player.replicanti.galaxies/2
+  if (player.timestudy.studies.includes(132)) galaxies += player.replicanti.galaxies*0.4
+  if (player.timestudy.studies.includes(225)) galaxies += Math.floor(player.replicanti.amount.e / 1000)
+  if (player.timestudy.studies.includes(226)) galaxies += Math.floor(player.replicanti.gal / 15)
+  galaxies += Math.min(player.replicanti.galaxies, player.replicanti.gal) * Math.max(Math.pow(Math.log10(player.infinityPower.plus(1).log10()+1), 0.03 * ECTimesCompleted("eterc8"))-1, 0)
+  if (galaxies < 3) {
       let baseMultiplier = 0.9;
       if (player.galaxies == 0) baseMultiplier = 0.89
       if (player.currentChallenge == "challenge6" || player.currentChallenge == "postc1") baseMultiplier = 0.93;
       let perGalaxy = 0.02;
-      let galaxies = player.galaxies+player.replicanti.galaxies+player.dilation.freeGalaxies
-      if (player.timestudy.studies.includes(133)) galaxies += player.replicanti.galaxies/2
-      if (player.timestudy.studies.includes(132)) galaxies += player.replicanti.galaxies*0.4
-      if (player.timestudy.studies.includes(225)) galaxies += Math.floor(player.replicanti.amount.e / 1000)
-      if (player.timestudy.studies.includes(226)) galaxies += Math.floor(player.replicanti.gal / 15)
-      galaxies += Math.min(player.replicanti.galaxies, player.replicanti.gal) * Math.max(Math.pow(Math.log10(player.infinityPower.plus(1).log10()+1), 0.03 * ECTimesCompleted("eterc8"))-1, 0)
       if (player.infinityUpgrades.includes("galaxyBoost")) perGalaxy *= 2;
       if (player.infinityUpgrades.includes("postGalaxy")) perGalaxy *= 1.5;
       if (player.challenges.includes("postc5")) perGalaxy *= 1.1;
@@ -27,12 +27,6 @@ function getTickSpeedMultiplier() {
       let baseMultiplier = 0.8
       if (player.currentChallenge == "challenge6" || player.currentChallenge == "postc1") baseMultiplier = 0.83
       let perGalaxy = 0.965
-      let galaxies = player.galaxies-2+player.replicanti.galaxies+player.dilation.freeGalaxies
-      if (player.timestudy.studies.includes(133)) galaxies += player.replicanti.galaxies/2
-      if (player.timestudy.studies.includes(132)) galaxies += player.replicanti.galaxies*0.4
-      if (player.timestudy.studies.includes(225)) galaxies += Math.floor(player.replicanti.amount.e / 1000)
-      if (player.timestudy.studies.includes(226)) galaxies += Math.floor(player.replicanti.gal / 15)
-      galaxies +=  Math.min(player.replicanti.galaxies, player.replicanti.gal) * Math.max(Math.pow(Math.log10(player.infinityPower.plus(1).log10()+1), 0.03 * ECTimesCompleted("eterc8"))-1, 0)
       if (player.infinityUpgrades.includes("galaxyBoost")) galaxies *= 2;
       if (player.infinityUpgrades.includes("postGalaxy")) galaxies *= 1.5;
       if (player.challenges.includes("postc5")) galaxies *= 1.1;

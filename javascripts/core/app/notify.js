617bf62828550cb43ca917379bdb1e2da673a4df
Andrei Andreev <Razenpok@outlook.com>
2018-11-14 22:10:37 +0300

    Separate animation from o-notification, Add notifications to theme preview

--- javascripts/core/app/notify.js
+++ javascripts/core/app/notify.js
@@ -2,16 +2,30 @@ ui.notify = function() {
   const container = document.getElementById("notification-container");
   const template = document.createElement('div');
   template.classList.add("o-notification");
+  const enterAnimation = "a-notification--enter";
+  const leaveAnimation = "a-notification--leave";
   function showNotification(text, elClass) {
     const el = template.cloneNode();
-    el.classList.add(elClass);
     el.textContent = text;
+    el.classList.add(elClass, enterAnimation);
     container.appendChild(el);
-    el.onclick = () => {
-      el.onclick = undefined;
-      el.style["animation-name"] = "o-notification--hide";
-    };
-    setTimeout(() => el.remove(), 2500);
+    let entered = false;
+    function stopEnter() {
+      if (entered) return;
+      entered = true;
+      el.classList.remove(enterAnimation);
+    }
+    setTimeout(() => stopEnter(), 500);
+    let leaving = false;
+    function leave() {
+      if (leaving) return;
+      leaving = true;
+      stopEnter();
+      el.classList.add(leaveAnimation);
+      setTimeout(() => el.remove(), 500);
+    }
+    setTimeout(() => leave(), 2000);
+    el.onclick = () => leave();
   }
   return {
     success: (text) => showNotification(text, "o-notification--success"),

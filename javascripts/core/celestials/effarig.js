ffe56a5ddd525b38a43625bc4fae989b0c04478d
IvarK <ivarkera@gmail.com>
2018-12-04 21:38:48 +0200

    fix shop threshold

--- javascripts/core/celestials/effarig.js
+++ javascripts/core/celestials/effarig.js
@@ -30,7 +30,7 @@ var Effarig = {
     if (!this.has(EFFARIG_UNLOCKS.RUN) && this.rmStore > 5e12) player.celestials.effarig.unlocks.push(EFFARIG_UNLOCKS.RUN)
     else if (!this.has(EFFARIG_UNLOCKS.EPGEN) && this.rmStore > 1e15) player.celestials.effarig.unlocks.push(EFFARIG_UNLOCKS.EPGEN)
     else if (!this.has(EFFARIG_UNLOCKS.TERESA) && this.rmStore > 1e18) player.celestials.effarig.unlocks.push(EFFARIG_UNLOCKS.TERESA)
-    else if (!this.has(EFFARIG_UNLOCKS.SHOP) && this.rmStore > 1e18) player.celestials.effarig.unlocks.push(EFFARIG_UNLOCKS.SHOP)
+    else if (!this.has(EFFARIG_UNLOCKS.SHOP) && this.rmStore > 1e24) player.celestials.effarig.unlocks.push(EFFARIG_UNLOCKS.SHOP)
   },
   has(id) {
     return player.celestials.effarig.unlocks.includes(id)

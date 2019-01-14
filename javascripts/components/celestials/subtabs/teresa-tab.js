91d2b72ceb97c8bf2cbb8a4628ac6a426a82fa9c
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2019-01-14 09:22:01 -0600

    More code readability

--- javascripts/components/celestials/subtabs/teresa-tab.js
+++ javascripts/components/celestials/subtabs/teresa-tab.js
@@ -78,19 +78,19 @@ Vue.component('teresa-tab', {
       <div class="c-teresa-relics">You have {{ shortenRateOfChange(relicShards) }} Relic Shards.</div>
       <div class="c-teresa-relic-description">You gain {{ shortenRateOfChange(shardsGained) }} Shards next reality, based on different kinds of glyph effects you have equipped and EP.</div>
       <div class="l-teresa-shop">
-        <button class="o-teresa-shop-button" @click="buyUnlock(0, 50000)" :class="{ 'teresa-unlock-bought': unlocks[0] }">Unlock glyph level adjustment.<br>Cost: 50,000 Relic Shards</button>
-        <button class="o-teresa-shop-button" @click="buyUnlock(1, 2e6)" :class="{ 'teresa-unlock-bought': unlocks[1] }">Unlock automatic glyph sacrifice.<br>Cost: 2,000,000 Relic Shards</button>
-        <button class="o-teresa-shop-button" @click="buyUnlock(2, 5e6)" :class="{ 'teresa-unlock-bought': unlocks[2] }">Unlock automatic glyph picker.<br>Cost: 5,000,000 Relic Shards</button>
+        <button class="o-teresa-shop-button" @click="buyUnlock(0, 50000)" :class="{ 'teresa-unlock-bought': Teresa.has(TERESA_UNLOCKS.ADJUSTER) }">Unlock glyph level adjustment.<br>Cost: 50,000 Relic Shards</button>
+        <button class="o-teresa-shop-button" @click="buyUnlock(1, 2e6)" :class="{ 'teresa-unlock-bought': Teresa.has(TERESA_UNLOCKS.AUTOSACRIFICE) }">Unlock automatic glyph sacrifice.<br>Cost: 2,000,000 Relic Shards</button>
+        <button class="o-teresa-shop-button" @click="buyUnlock(2, 5e6)" :class="{ 'teresa-unlock-bought': Teresa.has(TERESA_UNLOCKS.AUTOPICKER) }">Unlock automatic glyph picker.<br>Cost: 5,000,000 Relic Shards</button>
       </div>
-      <button class="o-teresa-shop-button" @click="buyUnlock(3, 1e7)" :class="{ 'teresa-unlock-bought': unlocks[3] }">Unlock Teresa's reality.<br>Cost: 10,000,000 Relic Shards</button>
+      <button class="o-teresa-shop-button" @click="buyUnlock(3, 1e7)" :class="{ 'teresa-unlock-bought': Teresa.has(TERESA_UNLOCKS.RUN) }">Unlock Teresa's reality.<br>Cost: 10,000,000 Relic Shards</button>
       <div class="l-teresa-glyph-settings">
-        <div v-if="unlocks[1]">
+        <div v-if="Teresa.has(TERESA_UNLOCKS.AUTOSACRIFICE)">
           Highest type will be picked, lowest sacrificed.
           <draggable :list="typePriorities" @end="move()">
             <div v-for="element in typePriorities" class="o-teresa-glyph-type">{{element}}</div>
           </draggable>
         </div>
-        <div v-if="unlocks[0]">
+        <div v-if="Teresa.has(TERESA_UNLOCKS.ADJUSTER)">
           <div>
             <glyph-weight-sliders v-model="weights.ep" name="EP" @input="adjustWeights('ep')"/>
             <glyph-weight-sliders v-model="weights.repl" name="Replicanti" @input="adjustWeights('repl')"/>
@@ -100,12 +100,12 @@ Vue.component('teresa-tab', {
             <glyph-weight-sliders v-model="weights.eternities" name="Eternities" @input="adjustWeights('eternities')"/>
           </div>
         </div>
-        <div v-if="unlocks[2]">Glyph effect weight settings here</div>
+        <div v-if="Teresa.has(TERESA_UNLOCKS.AUTOPICKER)">Glyph effect weight settings here</div>
       </div>
-      <div v-if="unlocks[3]"><button class="o-teresa-shop-button teresa-run-button" @click="startRun()">Start a new reality, all production and gamespeed is severely lowered and you can only use one glyph, infinity and time dimensions reduce the production penalty. IP multipliers are disabled. You will gain unlocks at Infinity, Eternity and Reality.</button>
-        <div v-if="unlocks[4]">Infinity: IP multipliers have partial effectiveness in Teresa Reality, infinitied stat raises the replicanti cap and increases your max RG.</div>
-        <div v-if="unlocks[5]">Eternity: You can now use 5 glyphs and production penalty is reduced in Teresa Reality; eternitied stat generates infinitied stat.</div>
-        <div v-if="unlocks[6]">Reality: (something outside)</div>
+      <div v-if="Teresa.has(TERESA_UNLOCKS.RUN)"><button class="o-teresa-shop-button teresa-run-button" @click="startRun()">Start a new reality, all production and gamespeed is severely lowered and you can only use one glyph, infinity and time dimensions reduce the production penalty. IP multipliers are disabled. You will gain unlocks at Infinity, Eternity and Reality.</button>
+        <div v-if="Teresa.has(TERESA_UNLOCKS.INFINITY_COMPLETE)">Infinity: IP multipliers have partial effectiveness in Teresa Reality, infinitied stat raises the replicanti cap and increases your max RG.</div>
+        <div v-if="Teresa.has(TERESA_UNLOCKS.ETERNITY_COMPLETE)">Eternity: You can now use 5 glyphs and production penalty is reduced in Teresa Reality; eternitied stat generates infinitied stat, unlocks The Enslaved Ones.</div>
+        <div v-if="Teresa.has(TERESA_UNLOCKS.REALITY_COMPLETE)">Reality: (Reward not implemented yet)</div>
       </div>
     </div>`
 });

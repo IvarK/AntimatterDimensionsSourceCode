eca8d5e364714fcbb77b64230bb777fb4a6f4b4d
garnet420 <none@none>
2019-01-19 10:48:39 -0500

    Add Array maptToObject to simplify dictionary making

--- javascripts/core/extensions.js
+++ javascripts/core/extensions.js
@@ -147,6 +147,21 @@ Array.prototype.last = function(predicate) {
 };
 
 /**
+ * @param {function?} keyFun
+ * @param {function?} valueFun
+ * @returns {object}
+ */
+Array.prototype.mapToObject = function(keyFun, valueFun) {
+  if (typeof keyFun !== "function" || typeof valueFun !== "function")
+    throw "keyFun and valueFun must be functions";
+  let out = {}
+  for (let elem of this) {
+    out[keyFun(elem)] = valueFun(elem);
+  }
+  return out;
+}
+
+/**
  * @type {number[]}
  */
 Array.dimensionTiers = Array.range(1, 8);

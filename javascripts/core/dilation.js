5e25d5f678f0874e2046ee1d362d9e5429d1585a
garnet420 <795869+garnet420@users.noreply.github.com>
2019-02-08 20:18:50 -0500

    fix implicit decimal to number cast in tachyon particle goal display

--- javascripts/core/dilation.js
+++ javascripts/core/dilation.js
@@ -114,13 +114,13 @@ function getTachyonGain() {
 
 function getTachyonReq() {
   let mult = DilationUpgrade.tachyonGain.effectValue;
-  if (player.reality.rebuyables[4] > 0) mult *= Math.pow(3, player.reality.rebuyables[4])
-  if (player.reality.upg.includes(8)) mult *= Math.sqrt(Math.pow(player.achPow, getAdjustedGlyphEffect("effarigachievement")))
-  if (player.reality.upg.includes(15)) mult *= Math.max(Math.sqrt(Decimal.log10(player.epmult)) / 3, 1)
+  if (player.reality.rebuyables[4] > 0) mult = mult.times(Math.pow(3, player.reality.rebuyables[4]))
+  if (player.reality.upg.includes(8)) mult = mult.times(Decimal.sqrt(player.achPow.pow(getAdjustedGlyphEffect("effarigachievement"))))
+  if (player.reality.upg.includes(15)) mult = mult.times(Math.max(Math.sqrt(Decimal.log10(player.epmult)) / 3, 1))
   let sacEffect = getGlyphSacEffect("dilation")
-  if (sacEffect > 1) mult *= sacEffect
+  if (sacEffect > 1) mult = mult.times(sacEffect)
   
-  let req = Decimal.pow(10, Math.pow(player.dilation.totalTachyonParticles * Math.pow(400, 1.5) / mult, 2/3))
+  let req = Decimal.pow(10, Decimal.pow(player.dilation.totalTachyonParticles.times(Math.pow(400, 1.5)).divideBy(mult), 2/3))
   return req
 }
 

12be3442baeec12fe6e3b12f5a504c45192a45b4
Ikerstreamer <ikerdeaguirre@hotmail.com>
2019-03-30 15:15:00 -0700

    Removed parseInt()

--- javascripts/core/notations.js
+++ javascripts/core/notations.js
@@ -911,10 +911,11 @@ Notation.Prime = new class PrimeNotation extends Notation {
       this._primeArrays.push(generatedArray.slice(i, Math.min(generatedArray.length, i + 100)));
     }
     // Find the smallest number in each of the sub arrays.
-    this._primeMin = this._primeArrays.map(x => parseInt(x[0]));
+    this._primeMin = this._primeArrays.map(x => x[0]);
     
     // Unicode characters for exponents ranging 0 - 13.
-    this._sup = ["\u2070", "\u00B9", "\u00B2", "\u00B3", "\u2074", "\u2075", "\u2076", "\u2077", "\u2078", "\u2079", "\u00B9\u2070", "\u00B9\u00B9", "\u00B9\u00B2", "\u00B9\u00B3"];
+    this._sup = ["\u2070", "\u00B9", "\u00B2", "\u00B3", "\u2074", "\u2075", "\u2076", "\u2077", "\u2078", 
+    "\u2079", "\u00B9\u2070", "\u00B9\u00B9", "\u00B9\u00B2", "\u00B9\u00B3"];
   }
 
   formatInfinite() {

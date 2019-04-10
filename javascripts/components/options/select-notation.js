b32a0e23c2aa36728c1dbe4f7949b6f238d52d9e
garnet420 <795869+garnet420@users.noreply.github.com>
2019-04-10 06:43:57 -0400

    add dropdown menu for notations (#449)
    * add dropdown menu for notations
    
    * fix codefactor issues
    
    * remove dead code, simplify select-notation data
    
    * remove unused functions in options.js
    
    * fix vue complaint about key in select-notation.js

---
+++ javascripts/components/options/select-notation.js
@@ -0,0 +1,14 @@
+Vue.component("select-notation", {
+  computed: {
+    notations: () => Notations.list,
+  },
+  template: `
+    <div class="l-select-notation">
+      <div v-for="notation in notations" :key="notation.name"
+           class="o-primary-btn l-select-notation__item c-select-notation__item"
+           @click="notation.setCurrent()">
+        {{notation.name}}
+      </div>
+    </div>
+  `
+});

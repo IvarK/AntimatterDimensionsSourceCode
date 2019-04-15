479a3ac29908f0196639d475d9f5daa6ac3fffca
Andrei Andreev <Razenpok@outlook.com>
2019-04-16 02:34:45 +0300

    Make shortcut list hint clickable

--- javascripts/components/options/options-tab.js
+++ javascripts/components/options/options-tab.js
@@ -2,6 +2,8 @@ Vue.component("options-tab", {
   template:
     `<div class="l-options-tab">
       <options-button-grid />
-      <p>Press <kbd>?</kbd> to open shortcut list.</p>
+      <p onclick="Modal.shortcuts.show()" class="c-options-tab__shortcuts-link">
+        Press <kbd>?</kbd> to open shortcut list.
+      </p>
     </div>`
 });

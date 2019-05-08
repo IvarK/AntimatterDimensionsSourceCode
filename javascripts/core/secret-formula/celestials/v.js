791abb66765e8947c26a9e9f66e6114eb43c5686
Dan Simon <dan@pipeline.com>
2019-05-08 19:45:10 -0400

    make matterception even harder

--- javascripts/core/secret-formula/celestials/v.js
+++ javascripts/core/secret-formula/celestials/v.js
@@ -48,7 +48,7 @@ GameDatabase.celestials.v = {
       id: 5,
       name: "Matterception",
       description: value => `Get ${value} Dimensional Boosts while dilating time, inside EC5.`,
-      values: [45, 48, 51, 54, 57, 60],
+      values: [50, 52, 54, 56, 58, 60],
       condition: x => player.dilation.active && EternityChallenge(5).isRunning && player.resets >= x
     }
   ]

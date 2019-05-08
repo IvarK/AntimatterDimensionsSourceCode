340a3d582ceb6795ef1a8f9bc7b1f91700683fda
IvarK <ivarkera@gmail.com>
2019-05-08 19:16:50 +0300

    big crunch functionality

--- javascripts/components/new-ui.js
+++ javascripts/components/new-ui.js
@@ -1,7 +1,17 @@
 Vue.component('new-ui', {
   data() {
     return {
-      view: ui.view
+      view: ui.view,
+      showCrunch: false
+    }
+  },
+  methods: {
+    update() {
+      this.showCrunch = !player.break && player.bestInfinityTime > 60 * 1000 && player.money.gte(Number.MAX_VALUE)
+      console.log(this.showCrunch)
+    },
+    infinity() {
+      bigCrunchReset()
     }
   },
   template:
@@ -9,7 +19,11 @@ Vue.component('new-ui', {
     <sidebar></sidebar>
     <div class="game-container">
       <div class="quote"><div id="news">.</div></div>
-      <component :is="$viewModel.page"></component>
+      <component :is="$viewModel.page" v-if="!showCrunch"></component>
+      <div v-else>
+        <h3>The world has collapsed due to excess antimatter.</h3>
+        <button class="btn-big-crunch" @click="infinity()">Big Crunch</button>
+      </div>
     </div>
     <div id="notification-container" class="l-notification-container"></div>
   </div>`

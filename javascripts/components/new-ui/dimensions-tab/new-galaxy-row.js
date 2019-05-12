fece1b82db18152168d7099ebc1af75f78a91693
IvarK <ivarkera@gmail.com>
2019-05-12 12:39:01 +0300

    fix galaxy error on new UI

--- javascripts/components/new-ui/dimensions-tab/new-galaxy-row.js
+++ javascripts/components/new-ui/dimensions-tab/new-galaxy-row.js
@@ -1,7 +1,9 @@
+"use strict";
+
 Vue.component('new-galaxy-row', {
   data() {
     return {
-      type: "",
+      type: GalaxyType.NORMAL,
       galaxies: {
         normal: 0,
         replicanti: 0,
@@ -12,21 +14,45 @@ Vue.component('new-galaxy-row', {
         amount: 0
       },
       canBeBought: false,
-      hasIncreasedScaling: false,
-      hasReplicantiGalaxies: false,
-      hasDilationGalaxies: false,
-      costScalingText: "",
-      lockMessage: null,
+      distantStart: 0,
+      lockText: null
     };
   },
   computed: {
     dimName() {
-      return SHORT_DISPLAY_NAMES[this.requirement.tier];
+      return DISPLAY_NAMES[this.requirement.tier];
     },
-    buttonMessage() {
-      return this.lockMessage
-        ? this.lockMessage
-        : "Lose all your previous progress, but get a tickspeed boost";
+    buttonText() {
+      return this.lockText === null
+        ? "Lose all your previous progress, but get a tickspeed boost"
+        : this.lockText;
+    },
+    sumText() {
+      const parts = [this.galaxies.normal];
+      if (this.galaxies.replicanti > 0) parts.push(this.galaxies.replicanti);
+      if (this.galaxies.dilation > 0) parts.push(this.galaxies.dilation);
+      return parts.map(shortenSmallInteger).join(" + ");
+    },
+    typeName() {
+      switch (this.type) {
+        case GalaxyType.NORMAL: return "Antimatter Galaxies";
+        case GalaxyType.DISTANT: return "Distant Antimatter Galaxies";
+        case GalaxyType.REMOTE: return "Remote Antimatter Galaxies";
+      }
+      return undefined;
+    },
+    hasIncreasedScaling() {
+      return this.type !== GalaxyType.NORMAL;
+    },
+    costScalingText() {
+      switch (this.type) {
+        case GalaxyType.DISTANT:
+          return `Each galaxy is more expensive past ${this.distantStart} galaxies`;
+        case GalaxyType.REMOTE:
+          return "Increased galaxy cost scaling: " +
+            `Quadratic past ${this.distantStart} (distant), exponential past 800 (remote)`;
+      }
+      return undefined;
     }
   },
   methods: {
@@ -39,51 +65,27 @@ Vue.component('new-galaxy-row', {
       this.requirement.amount = requirement.amount;
       this.requirement.tier = requirement.tier;
       this.canBeBought = requirement.isSatisfied && Galaxy.canBeBought;
-      this.hasReplicantiGalaxies = this.galaxies.replicanti !== 0;
-      this.hasDilationGalaxies = this.galaxies.dilation !== 0;
-      if (Galaxy.canBeBought) {
-        this.lockMessage = null;
-      } else if (EternityChallenge(6).isRunning) {
-        this.lockMessage = "Locked (Eternity Challenge 6)";
-      } else if (InfinityChallenge(7).isRunning) {
-        this.lockMessage = "Locked (Infinity Challenge 7)";
-      } else if (NormalChallenge(8).isRunning) {
-        this.lockMessage = "Locked (8th Dimension Autobuyer Challenge)";
-      } else {
-        this.lockMessage = null;
-      }
-      this.updateCostScaling();
+      this.distantStart = EternityChallenge(5).isRunning ? 0 : Galaxy.costScalingStart;
+      this.lockText = this.generateLockText();
     },
-    secondSoftReset() {
-      galaxyResetBtnClick();
-    },
-    updateCostScaling() {
-      const distantStart = EternityChallenge(5).isRunning ? 0 : Galaxy.costScalingStart;
-      this.hasIncreasedScaling = player.galaxies > distantStart;
-      if (Galaxy.type.startsWith("Distant")) {
-        this.costScalingText = `Each galaxy is more expensive past ${distantStart} galaxies`;
-        return;
-      }
-      if (Galaxy.type.startsWith("Remote")) {
-        const remoteStart = 800;
-        this.costScalingText = "Increased galaxy cost scaling: " +
-          `Quadratic past ${distantStart} (distant), exponential past ${remoteStart} (remote)`;
-        return;
-      }
-      this.costScalingText = "";
+    generateLockText() {
+      if (Galaxy.canBeBought) return null;
+      if (EternityChallenge(6).isRunning) return "Locked (Eternity Challenge 6)";
+      if (InfinityChallenge(7).isRunning) return "Locked (Infinity Challenge 7)";
+      if (NormalChallenge(8).isRunning) return "Locked (8th Dimension Autobuyer Challenge)";
+      return null;
     }
   },
   template:
   `<div class="reset-container galaxy">
-    <h4>{{type}} ({{shortenSmallInteger(galaxies.normal)}}<span v-if="hasReplicantiGalaxies"> + {{shortenSmallInteger(galaxies.replicanti)}}</span><span v-if="hasDilationGalaxies"> + {{shortenSmallInteger(galaxies.dilation)}}</span>)
-    </h4>
+    <h4>{{typeName}} ({{sumText}})</h4>
     <span>Requires: {{shortenSmallInteger(requirement.amount)}} {{dimName}} D</span>
     <div v-if="hasIncreasedScaling">{{costScalingText}}</div>
     <button 
       class="storebtn" style="height: 56px; font-size: 1rem;"
       :class="{ 'storebtn-unavailable': !canBeBought }"
-      @click="secondSoftReset"
+      onclick="galaxyResetBtnClick()"
       :enabled="canBeBought"
-    >{{buttonMessage}}</button>
+    >{{buttonText}}</button>
   </div>`
 })

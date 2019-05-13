c44971462bbd440d9b20f146c2b73ca6f4db7b4d
Dan Simon <dan@pipeline.com>
2019-05-13 12:32:25 -0400

    remove repetition

--- javascripts/core/notations/shi.js
+++ javascripts/core/notations/shi.js
@@ -3,7 +3,7 @@
 Notation.shi = new class ShiNotation extends Notation {
   constructor() {
     super("Shi");
-    this._shiChars = "施氏食獅史石室詩士施氏嗜獅誓食十獅氏時時適市視獅十時適十獅適市是時適施氏適是市氏視是十獅恃矢勢使是十獅逝世氏拾是十獅屍適石室石室濕氏使侍拭石室石室拭氏始試食十獅屍食時始識十獅屍實十石獅屍試釋是";
+    this._shiChars = "世使侍勢十史嗜士始室實屍市恃拭拾施是時氏濕獅矢石視試詩誓識逝適釋食";
   }
 
   get isPainful() {
@@ -23,7 +23,7 @@ Notation.shi = new class ShiNotation extends Notation {
   }
 
   getShiCharacter(x) {
-    return this._shiChars[Math.round(x) % this._shiChars.length];
+    return this._shiChars[Math.floor(x) % this._shiChars.length];
   }
 
   /**
@@ -31,7 +31,7 @@ Notation.shi = new class ShiNotation extends Notation {
    * @return {string}
    */
   shi(value) {
-    const scaled = value.pLog10() * 10;
-    return Array.range(0, 4).map(i => this.getShiCharacter(Math.pow(scaled, Math.pow(2, -i)))).join("");
+    const scaled = Math.pow(value.pLog10() * 1000, 0.1);
+    return Array.range(0, 4).map(i => this.getShiCharacter(scaled * Math.pow(this._shiChars.length, i))).join("");
   }
 }();

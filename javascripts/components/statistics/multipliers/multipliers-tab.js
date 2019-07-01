299717d673568bb9af003e427a771af3baf5723f
earth <divine.icbm@gmail.com>
2019-07-01 13:54:00 -0500

    Update multipliers-tab.js

--- javascripts/components/statistics/multipliers/multipliers-tab.js
+++ javascripts/components/statistics/multipliers/multipliers-tab.js
@@ -9,6 +9,7 @@ Vue.component("multipliers-tab", {
     <h3>Normal Dimension Multipliers</h3>
     <div><b>Base multiplier:</b> {{ }}×</div>
     <div><b>Buy10 multiplier:</b> {{ }}×</div>
+    <div><b>Dimension Shift/Boost multiplier:</b> {{ }}×</div>
     </div>
 
     </div>

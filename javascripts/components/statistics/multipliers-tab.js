b2f1045c3b7264d73ada9684f9d11e02ecabb050
earth <divine.icbm@gmail.com>
2019-07-01 12:12:40 -0500

    Update multipliers-tab.js

--- javascripts/components/statistics/multipliers-tab.js
+++ javascripts/components/statistics/multipliers-tab.js
@@ -1,6 +1,17 @@
 "use strict";
 
 Vue.component("multipliers-tab", {
-    
+    template:
+    `<div>
+    <br>
+    <button class="storebtn">Dimensions</button>
+    <div>
+    <h3>Normal Dimension Multipliers</h3>
+    <div><b>Base multiplier:</b> {{ }}×</div>
+    <div><b>Buy10 multiplier:</b> {{ }}×</div>
+    </div>
+
+    </div>
+`
 }
     )

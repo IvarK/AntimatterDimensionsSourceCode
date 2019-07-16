0b293dac03863f9ae7b86cc366390aeb4fe7145e
garnet420 <795869+garnet420@users.noreply.github.com>
2019-07-16 17:44:20 -0400

    add automato support for prestige autobuyers at fixed currency values

--- javascripts/core/automator/parser.js
+++ javascripts/core/automator/parser.js
@@ -129,6 +129,11 @@ const AutomatorGrammar = (function() {
         $.CONSUME(T.Last);
       });
 
+      $.RULE("currencyAmount", () => {
+        $.CONSUME(T.NumberLiteral);
+        $.CONSUME(T.Currency);
+      });
+
       // Very important to call this after all the rules have been setup.
       // otherwise the parser may not work correctly as it will lack information
       // derived from the self analysis.

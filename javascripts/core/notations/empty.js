6a6f4cba262cf6d90e6ec236aef35561eac3569e
earth <divine.icbm@gmail.com>
2019-07-19 21:31:08 -0500

    optimize empty

--- javascripts/core/notations/empty.js
+++ javascripts/core/notations/empty.js
@@ -4,7 +4,6 @@ Notation.empty = new class EmptyNotation extends Notation {
   constructor() {
     super("Empty");
     this.EMPTY = [" ", " ", " ", " ", " ", " ", " ", " "];
-    this.LOG8 = Math.log(8);
   }
 
   get isPainful() {
@@ -15,20 +14,6 @@ Notation.empty = new class EmptyNotation extends Notation {
   }
 
   formatDecimal(value) {
-    const log8 = Math.LN10 / this.LOG8 * value.log10();
-    let wholeLog = Math.floor(log8);
-    const decimalLog = log8 - wholeLog;
-    const decimalLog64 = Math.floor(decimalLog * 64);
-    const parts = [
-      this.EMPTY[decimalLog64 % 8],
-      this.EMPTY[Math.floor(decimalLog64 / 8)],
-    ];
-    while (wholeLog >= 8) {
-      const remainder = wholeLog % 8;
-      wholeLog = (wholeLog - remainder) / 8;
-      parts.push(this.EMPTY[remainder]);
-    }
-    parts.push(this.EMPTY[wholeLog]);
     return (" ");
   }
 }();

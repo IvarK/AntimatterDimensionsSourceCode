d3b8113cd0738b4e832e407111de6f338098b502
IvarK <ivarkera@gmail.com>
2019-07-27 01:36:47 +0300

    new ui infinity and eternity button stuff

--- javascripts/components/new-ui/sidebar-resources/sidebar-ep.js
+++ javascripts/components/new-ui/sidebar-resources/sidebar-ep.js
@@ -4,7 +4,18 @@ Vue.component("sidebar-ep", {
   data() {
     return {
       ep: new Decimal(0),
-      gained: new Decimal(0)
+      type: EPButtonDisplayType.FIRST_TIME,
+      showEternity: false,
+      gainedEP: new Decimal(0),
+      currentEPPM: new Decimal(0),
+      peakEPPM: new Decimal(0),
+      gainedTachyons: new Decimal(0),
+      challengeCompletions: 0,
+      gainedCompletions: 0,
+      fullyCompleted: false,
+      failedRestriction: undefined,
+      hasMoreCompletions: false,
+      nextGoalAt: new Decimal(0)
     };
   },
   props: {
@@ -13,15 +24,76 @@ Vue.component("sidebar-ep", {
   methods: {
     update() {
       this.ep.copyFrom(player.eternityPoints);
+      this.showEternity = player.infinityPoints.gte(Player.eternityGoal)
+      if (!this.showEternity) return;
+      if (player.eternities === 0) {
+        this.type = EPButtonDisplayType.FIRST_TIME;
+        return;
+      }
+
+      if (EternityChallenge.isRunning) {
+        if (!Perk.studyECBulk.isBought) {
+          this.type = EPButtonDisplayType.CHALLENGE;
+          return;
+        }
+        this.type = EPButtonDisplayType.CHALLENGE_RUPG;
+        this.updateChallengeWithRUPG();
+        return;
+      }
+
+      const gainedEP = gainedEternityPoints();
+      this.gainedEP.copyFrom(gainedEP);
+      const hasNewContent = player.realities === 0 &&
+        player.eternityPoints.exponent >= 4000 &&
+        player.timestudy.theorem.gt(5e9) &&
+        player.replicanti.amount.exponent > 20000;
+
+      if (player.dilation.active) {
+        this.type = hasNewContent
+          ? EPButtonDisplayType.DILATION_EXPLORE_NEW_CONTENT
+          : EPButtonDisplayType.DILATION;
+        this.gainedTachyons.copyFrom(getTachyonGain());
+        return;
+      }
+
+      this.type = hasNewContent
+        ? EPButtonDisplayType.NORMAL_EXPLORE_NEW_CONTENT
+        : EPButtonDisplayType.NORMAL;
+      this.currentEPPM.copyFrom(gainedEP.dividedBy(Time.thisEternity.totalMinutes));
+      this.peakEPPM.copyFrom(EPminpeak);
+    },
+    updateChallengeWithRUPG() {
+      const ec = EternityChallenge.current;
+      this.fullyCompleted = ec.isFullyCompleted;
+      if (this.fullyCompleted) return;
+      const status = ec.gainedCompletionStatus;
+      this.gainedCompletions = status.gainedCompletions;
+      this.failedRestriction = status.failedRestriction;
+      this.hasMoreCompletions = status.hasMoreCompletions;
+      this.nextGoalAt.copyFrom(status.nextGoalAt);
+    },
+    eternity() {
+      if (this.showEternity) {
+        eternity();
+      }
     }
   },
+  computed: {
+    eternityPeaks() {
+      return `${shorten(this.currentEPPM, 2, 0)} EP/min<br>Peaked at ${shorten(this.peakEPPM, 2, 0)} EP/min`
+    },
+  },
   template:
-  `<div class="resource">
+  `<div
+    class="resource"
+    @click="eternity"
+    :class=" { 'resource-eternity-canreset': showEternity }"
+    v-tooltip="eternityPeaks">
     <div v-if="cond">
       <h2 id="ep">{{ shorten(ep, 2, 0) }}</h2>
       <div class="resource-information">
         <span class="resource-name">Eternity Points</span>
-        <span class="resource-per-second"> +{{ shorten(gained) }}</span>
+        <span v-if="showEternity" class="resource-per-second"> +{{ shorten(gainedEP, 2, 0) }}</span>
       </div>
     </div>
   </div>`
--- javascripts/components/new-ui/sidebar-resources/sidebar-ip.js
+++ javascripts/components/new-ui/sidebar-resources/sidebar-ip.js
@@ -5,7 +5,9 @@ Vue.component("sidebar-ip", {
     return {
       ip: new Decimal(0),
       gained: new Decimal(0),
-      showCrunch: false
+      showCrunch: false,
+      peakIPPM: new Decimal(0),
+      currentIPPM: new Decimal(0)
     };
   },
   props: {
@@ -14,22 +16,36 @@ Vue.component("sidebar-ip", {
   methods: {
     update() {
       this.ip.copyFrom(player.infinityPoints);
-      this.gained.copyFrom(gainedInfinityPoints());
-      this.showCrunch = player.antimatter.gte(Number.MAX_VALUE) && player.break;
+      this.showCrunch = player.break && player.antimatter.gte(Decimal.MAX_NUMBER) &&
+      !NormalChallenge.isRunning && !InfinityChallenge.isRunning;
+      if (!this.showCrunch) return
+      const gainedIP = gainedInfinityPoints();
+      this.gained.copyFrom(gainedIP);
+      this.peakIPPM.copyFrom(IPminpeak);
+      this.currentIPPM.copyFrom(gainedIP.dividedBy(Time.thisInfinity.totalMinutes));
     },
     infinity() {
       if (this.showCrunch) {
         bigCrunchResetRequest();
       }
-    }
+    },
+  },
+  computed: {
+    crunchPeaks() {
+      return `${shorten(this.currentIPPM, 2, 0)} IP/min<br>Peaked at ${shorten(this.peakIPPM, 2, 0)} IP/min`
+    },
   },
   template:
-  `<div class="resource" @click="infinity" :class=" { 'resource-infinity-canreset': showCrunch }">
+  `<div 
+    class="resource" 
+    @click="infinity"
+    :class=" { 'resource-infinity-canreset': showCrunch }"
+    v-tooltip="crunchPeaks">
     <div v-if="cond">
       <h2 id="ip">{{ shorten(ip, 2, 0) }}</h2>
       <div class="resource-information">
         <span class="resource-name">{{ showCrunch ? "Infinity now for" : "Infinity Points" }}</span>
-        <span v-if="showCrunch" class="resource-per-second"> +{{ shorten(gained) }} IP</span>
+        <span v-if="showCrunch" class="resource-per-second"> +{{ shorten(gained, 2, 0) }} IP</span>
       </div>
     </div>
   </div>`

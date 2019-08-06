cec0984dfc66e2e8017ae826edf0cc6cbaf6853d
IvarK <ivarkera@gmail.com>
2019-08-06 14:08:35 +0300

    fix tickspeed row and achievements text

--- javascripts/components/new-ui/new-ui.js
+++ javascripts/components/new-ui/new-ui.js
@@ -4,12 +4,16 @@ Vue.component("new-ui", {
   data() {
     return {
       view: ui.view,
-      showCrunch: false
+      showCrunch: false,
+      breakInf: false,
+      realities: 0
     };
   },
   methods: {
     update() {
       this.showCrunch = !player.break && player.antimatter.gte(Number.MAX_VALUE);
+      this.breakInf = player.break;
+      this.realities = player.realities;
     }
   },
   template:
@@ -19,10 +23,10 @@ Vue.component("new-ui", {
     <div class="game-container">
       <news-ticker />
       <div class="tab-container">
-        <div class="l-reset-buttons-container">
-          <game-header-big-crunch-button/>
-          <reality-button/>
+        <div class="l-reset-buttons-container" v-if="breakInf">
           <game-header-eternity-button/>
+          <reality-button v-if="realities > 0"/>
+          <game-header-big-crunch-button/>
         </div>
         <div v-if="showCrunch">
           <h3>The world has collapsed due to excess antimatter.</h3>

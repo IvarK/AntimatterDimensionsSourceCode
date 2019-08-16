4994d3ef7d00a9d7f7b7fbae5fd75fffe94f62bf
IvarK <ivarkera@gmail.com>
2019-08-16 13:31:04 +0300

    delete files that came back with merge

--- javascripts/components/hint-text.js
+++
@@ -1,9 +0,0 @@
-Vue.component("hint-text", {
-  computed: {
-    isVisible: function() {
-      return ui.view.shiftDown;
-    }
-  },
-  template:
-    `<div v-show="isVisible" class="o-hint-text l-hint-text"><slot /></div>`
-});
--- javascripts/components/secondary-tab-button.js
+++
@@ -1,6 +0,0 @@
-Vue.component('secondary-tab-button', {
-  template:
-    `<button class="o-btn o-btn--secondary-tab" v-bind="$attrs" v-on="$listeners">
-      <slot/>
-    </button>`
-});

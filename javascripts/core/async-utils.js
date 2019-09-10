221c01b23e911e2587cd6c6e68a954e442c24da4
garnet420 <795869+garnet420@users.noreply.github.com>
2019-09-10 15:34:36 -0400

    add documentation to async-utils

--- javascripts/core/async-utils.js
+++ javascripts/core/async-utils.js
@@ -1,5 +1,10 @@
 "use strict";
 
+/**
+ * Async is used for making a big pile of computation into a manageable
+ * set of batches that don't lock up the UI.
+ * run() is the nominal entry point.
+ */
 const Async = {
   runForTime(fun, maxIter, config) {
     const batchSize = config.batchSize || 1;
@@ -15,6 +20,27 @@ const Async = {
     return 0;
   },
   sleepPromise: ms => new Promise(resolve => setTimeout(resolve, ms)),
+  /**
+   * Asynchronously run the specified function maxIter times, letting the event
+   * loop run periodically. The function is run in chunks of config.batchSize;
+   * when the elapsed time reaches a specified amount, execution will pause for
+   * config.sleepTime
+   * @param {function} fun Function to run (e.g. do some computation)
+   * @param {number} maxIter Total number of times to run the function
+   * @param {Object} config Options for how to do the calculation
+   * @param {Number} config.maxTime Max time, ms, over which to run continuously
+   * @param {Number} [config.batchSize] Number of times to run fun between time checks. Since Date.now() takes
+   *    non-zero time to execute, you don't necessarily want to check every iteration
+   * @param {Number} [config.sleepTime] Amount of time to suspend between computing
+   * @param {function(Number)} [config.asyncEntry] IF CALCULATION ISN'T DONE IN ONE BATCH, then this
+   *    gets called before the first sleep. Use this to set up a progress bar, for example. The function will
+   *    get passed the number of iterations run so far.
+   * @param {function(Number)} [config.asyncProgress] Called after the second and subsequent batches, with the
+   *    total number of iterations done thus far
+   * @param {function} [config.asyncExit] If more than one batch was done, this will be called. For example, can
+   *    be used to hide a progress bar.
+   * @returns {Promise}
+   */
   async run(fun, maxIter, config) {
     let remaining = this.runForTime(fun, maxIter, config);
     const sleepTime = config.sleepTime || 1;

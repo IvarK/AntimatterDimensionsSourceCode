a8e75b5e35262a29bd5cc35cb841e41572fa3977
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2019-10-06 13:14:45 -0500

    Add secret theme notification
    Fixes #227

--- javascripts/core/app/options.js
+++ javascripts/core/app/options.js
@@ -30,13 +30,13 @@ class GameOptions {
   }
 }
 
-let secretImports = [
+const secretImports = [
   "80b7fdc794f5dfc944da6a445a3f21a2d0f7c974d044f2ea25713037e96af9e3",
   "857876556a230da15fe1bb6f410ca8dbc9274de47c1a847c2281a7103dd2c274"
 ];
 
 function secretImportIndex(data) {
-  const sha = sha512_256(data.replace(/\s/g, '').toUpperCase());
+  const sha = sha512_256(data.replace(/\s/gu, "").toUpperCase());
   return secretImports.indexOf(sha);
 }
 
@@ -45,7 +45,7 @@ function isSecretImport(data) {
 }
 
 function tryImportSecret(data) {
-  let index = secretImportIndex(data);
+  const index = secretImportIndex(data);
   if (index === 0) {
     document.body.style.animation = "barrelRoll 5s 1";
     SecretAchievement(15).unlock();

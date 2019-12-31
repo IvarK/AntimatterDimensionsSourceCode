4943f2edb95b02536eafc6ff207c61b34f9a1935
Dan Simon <dan@pipeline.com>
2019-12-31 12:09:21 -0500

    Change shortenDimensions to shorten

--- javascripts/components/old-ui/header/game-header-amounts-line.js
+++ javascripts/components/old-ui/header/game-header-amounts-line.js
@@ -21,7 +21,7 @@ Vue.component("game-header-amounts-line", {
       }
     },
     shortenPoints(points) {
-      return this.shortenDimensions(points);
+      return this.shorten(points, 2, 0);
     }
   },
   template:

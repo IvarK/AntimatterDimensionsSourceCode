8269500993278fb138cc7a3450a0b3f9f9ee6f7e
Andrei Andreev <Razenpok@outlook.com>
2020-01-03 23:28:50 +0300

    Fix poopoo

--- javascripts/components/dimensions/normal/normal-dim-row.js
+++ javascripts/components/dimensions/normal/normal-dim-row.js
@@ -69,7 +69,7 @@ Vue.component("normal-dim-row", {
   template:
     `<div v-show="isUnlocked" class="c-normal-dim-row">
       <div class="c-normal-dim-row__name c-normal-dim-row__label">
-        {{name}} Dimension {{format(multiplierX, 1, 1)}}
+        {{name}} Dimension {{formatX(multiplier, 1, 1)}}
       </div>
       <div class="c-normal-dim-row__label c-normal-dim-row__label--growable">
         {{amountDisplay}} ({{formatInt(boughtBefore10)}}){{rateOfChangeDisplay}}

IvarK/AntimatterDimensionsSourceCode

commit 21b1a280d12b354c8e05abc2d755cccd4fae020e

Fix bug where Teresa container shows as empty in certain notations.
Also make Teresa container and bottom progress bar empty in Blind
notation. Fixes #1072.

javascripts/components/dimensions/normal/normal-dim-tab-progress-bar.js
@@ -3,17 +3,17 @@
 Vue.component("normal-dim-tab-progress-bar", {
   data() {
     return {
-      fill: new Decimal(0),
+      fill: 0,
       tooltip: ""
     };
   },
   computed: {
-    percents() {
-      return `${this.fill.toFixed(2)}%`;
+    displayPercents() {
+      return formatPercents(this.fill, 2);
     },
     progressBarStyle() {
       return {
-        width: this.percents
+        width: Notations.current.name === 'Blind' ? '0%' : `${this.fill.toFixed(2)}%`
       };
     }
   },
@@ -46,7 +46,7 @@ Vue.component("normal-dim-tab-progress-bar", {
   template:
     `<div class="c-progress-bar">
         <div :style="progressBarStyle" class="c-progress-bar__fill">
-            <span v-tooltip="tooltip" class="c-progress-bar__percents">{{percents}}</span>
+            <span v-tooltip="tooltip" class="c-progress-bar__percents">{{displayPercents}}</span>
           </div>
     </div>`
 });

javascripts/components/celestials/subtabs/teresa/teresa-tab.js
@@ -39,7 +39,7 @@ Vue.component("teresa-tab", {
       }
       this.time = now;
       this.rmStore = player.celestials.teresa.rmStore;
-      this.percentage = formatPercents(Teresa.fill, 2);
+      this.percentage = Notations.current.name === 'Blind' ? '0%' : `${(Teresa.fill * 100).toFixed(2)}%`;
       this.rmMult = Teresa.rmMultiplier;
       this.hasReality = Teresa.has(TERESA_UNLOCKS.RUN);
       this.hasEPGen = Teresa.has(TERESA_UNLOCKS.EPGEN);

commit f9ba4b1fd7c179dbc7dd602e70d4fab7552e6436

Fix bugs in progress bar that stopped it from displaying
These bugs were caused by https://github.com/IvarK/HahaSlabWontGetHere/commit/21b1a280d12b354c8e05abc2d755cccd4fae020e

javascripts/components/dimensions/normal/normal-dim-tab-progress-bar.js
@@ -13,14 +13,14 @@ Vue.component("normal-dim-tab-progress-bar", {
     },
     progressBarStyle() {
       return {
-        width: Notations.current.name === 'Blind' ? '0%' : `${this.fill.toFixed(2)}%`
+        width: Notations.current.name === 'Blind' ? '0%' : `${(this.fill * 100).toFixed(2)}%`
       };
     }
   },
   methods: {
     update() {
       const setProgress = (current, goal, tooltip) => {
-        this.fill.copyFrom(Decimal.min(current.pLog10() / Decimal.log10(goal) * 100, 100));
+        this.fill = Math.min(current.pLog10() / Decimal.log10(goal), 1);
         this.tooltip = tooltip;
       };
       const challenge = NormalChallenge.current || InfinityChallenge.current;
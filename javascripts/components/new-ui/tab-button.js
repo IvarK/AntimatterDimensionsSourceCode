bef783f5f39846aa90a317171f3df111c9caa38a
IvarK <ivarkera@gmail.com>
2020-04-11 20:42:37 +0300

    fix codefactor issue

--- javascripts/components/new-ui/tab-button.js
+++ javascripts/components/new-ui/tab-button.js
@@ -38,10 +38,13 @@ Vue.component("tab-button", {
     <div v-else class="l-tab-btn-inner">???</div>
     <div class="subtabs" v-if="isAvailable && subtabVisibilities.filter(x => x).length > 1">
       <div v-for="(subtab, index) in tab.subtabs"
-          v-if="subtabVisibilities[index]"
-          class="o-tab-btn o-tab-btn--subtab"
-          :class="tab.config.UIClass"
-          @click="subtab.show()"><span v-html="subtab.symbol"> <i v-if="subtab.hasNotification" class="fas fa-exclamation"></i></span>
+        v-if="subtabVisibilities[index]"
+        class="o-tab-btn o-tab-btn--subtab"
+        :class="tab.config.UIClass"
+        @click="subtab.show()">
+        <span v-html="subtab.symbol">
+          <i v-if="subtab.hasNotification" class="fas fa-exclamation"></i>
+        </span>
       </div>
     </div>
   </div>`

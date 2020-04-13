ea5e4c7ef4b671e5079215a46c1f89645cf2f06c
Omsi <omsi2000@gmail.com>
2020-04-12 17:01:44 -0700

    move reality ab mode toggle to inside reality ab div

--- javascripts/components/infinity/autobuyers/reality-autobuyer-box.js
+++ javascripts/components/infinity/autobuyers/reality-autobuyer-box.js
@@ -3,22 +3,56 @@
 Vue.component("reality-autobuyer-box", {
   data() {
     return {
+      mode: AUTO_REALITY_MODE.RM,
       levelCap: 0,
       isOverCap: false
     };
   },
   computed: {
-    autobuyer: () => Autobuyer.reality
+    autobuyer: () => Autobuyer.reality,
+    modes: () => [
+      AUTO_REALITY_MODE.RM,
+      AUTO_REALITY_MODE.GLYPH,
+      AUTO_REALITY_MODE.EITHER,
+      AUTO_REALITY_MODE.BOTH
+    ]
   },
   methods: {
     update() {
+      this.mode = this.autobuyer.mode;
       this.levelCap = Glyphs.levelCap;
       this.isOverCap = this.autobuyer.glyph > this.levelCap;
+    },
+    modeName(mode) {
+      switch (mode) {
+        case AUTO_REALITY_MODE.RM: return "Reality Machines";
+        case AUTO_REALITY_MODE.GLYPH: return "Glyph level";
+        case AUTO_REALITY_MODE.EITHER: return "Either";
+        case AUTO_REALITY_MODE.BOTH: return "Both";
+      }
+      throw new Error("Unknown auto reality mode");
+    },
+    changeMode(event) {
+      const mode = parseInt(event.target.value, 10);
+      this.autobuyer.mode = mode;
+      this.mode = mode;
     }
   },
   template:
     `<autobuyer-box :autobuyer="autobuyer" name="Automatic Reality">
       <template slot="intervalSlot">
+        <select
+          class="c-autobuyer-box__mode-select"
+          @change="changeMode"
+          >
+          <option
+            v-for="optionMode in modes"
+            :value="optionMode"
+            :selected="mode === optionMode"
+          >{{modeName(optionMode)}}</option>
+        </select>
+      </template>
+      <template slot="toggleSlot">
         <div>Target reality machines:</div>
         <autobuyer-input
          :autobuyer="autobuyer"
@@ -26,7 +60,7 @@ Vue.component("reality-autobuyer-box", {
          property="rm"
         />
       </template>
-      <template slot="toggleSlot">
+      <template slot="prioritySlot">
         <div>Target glyph level:</div>
         <autobuyer-input
          :autobuyer="autobuyer"

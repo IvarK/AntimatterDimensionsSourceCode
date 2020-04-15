90bbde0d3cd425987d87387ab4f4bd8b71c2b724
Dan Simon <dan@pipeline.com>
2020-04-15 17:34:01 -0400

    Fix save migration unlocking reality upgrades

--- javascripts/core/storage/migrations.js
+++ javascripts/core/storage/migrations.js
@@ -139,9 +139,6 @@ GameStorage.migrations = {
       GameStorage.migrations.setTutorialState(player);
 
       kong.migratePurchases();
-      
-      // Needed to check some reality upgrades which are usually only checked on eternity.
-      EventHub.dispatch(GAME_EVENT.SAVE_CONVERTED_FROM_PREVIOUS_VERSION);
     }
   },
 

5188957e1963c248fb9deb3b2e79e0249f6a1620
IvarK <ivarkera@gmail.com>
2020-04-16 10:45:49 +0300

    apply to new dim row also

--- javascripts/components/new-ui/infinity-dimensions-tab/new-inf-dimension-row.js
+++ javascripts/components/new-ui/infinity-dimensions-tab/new-inf-dimension-row.js
@@ -41,11 +41,15 @@ Vue.component("new-inf-dimension-row", {
     },
     costDisplay() {
       const requirement = InfinityDimension(this.tier).requirement;
-      if (!this.isUnlocked) {
-        if (this.requirementReached) return "Unlock";
-        return `Reach ${format(requirement)} AM`;
+      if (this.isUnlocked) {
+        return this.isCapped ? "Capped!" : `Cost: ${format(this.cost)} IP`;
       }
-      return this.isCapped ? "Capped!" : `Cost: ${format(this.cost)} IP`;
+      
+      if (this.requirementReached) {
+        return "Unlock";
+      }
+
+      return `Reach ${format(requirement)} AM`;
     },
     capTooltip() {
       return this.isCapped

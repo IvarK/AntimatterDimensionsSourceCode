fee8421b28ce9346f9376ffd1132661b0a727d34
IvarK <ivarkera@gmail.com>
2020-04-18 21:06:14 +0300

    fix st cost being visible pre-V

--- javascripts/components/eternity/time-studies/normal-time-study.js
+++ javascripts/components/eternity/time-studies/normal-time-study.js
@@ -52,7 +52,7 @@ Vue.component("normal-time-study", {
                           ? this.study.config.requirement()
                           : TimeStudy(this.study.config.requirement).isBought;
 
-      this.showSTCost = !canBeBought &&
+      this.showSTCost = !canBeBought && Achievement(151).isUnlocked &&
                         this.study.config.requirementV !== undefined &&
                         this.study.config.requirementV() &&
                         this.study.STCost !== undefined;

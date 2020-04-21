0442101511d177a81aa171fd6b5ab0d6029ed096
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2020-04-21 18:04:31 -0500

    Minor visual fixes

--- javascripts/components/new-ui/new-ui.js
+++ javascripts/components/new-ui/new-ui.js
@@ -50,7 +50,7 @@ Vue.component("new-ui", {
       if (isChallengePowerVisible) {
         const powerArray = [];
         if (isC2Running) powerArray.push(`Production: ${formatPercents(player.chall2Pow, 2, 2)}`);
-        if (isC3Running) powerArray.push(`First dimension: ${formatX(player.chall3Pow, 2, 2)}`);
+        if (isC3Running) powerArray.push(`First dimension: ${formatX(player.chall3Pow, 3, 4)}`);
         if (isIC6Running) powerArray.push(`Matter: /
           ${format(new Decimal(1).timesEffectOf(InfinityChallenge(6)), 2, 2)}`);
         if (isIC8Running) powerArray.push(`Production: /

0a4fa1e7ce6857a925cd35db2412c4b9ea0988db
Omsi <omsi2000@gmail.com>
2020-04-21 12:03:35 -0700

    review fixes

--- javascripts/game.js
+++ javascripts/game.js
@@ -746,7 +746,7 @@ function simulateTime(seconds, real, fast) {
   // into a higher diff per tick
   // warning: do not call this function with real unless you know what you're doing
   // calling it with fast will only simulate it with a max of 50 ticks
-  let ticks = seconds * 20;
+  let ticks = Math.floor(seconds * 20);
   GameUI.notify.showBlackHoles = false;
 
   // Limit the tick count (this also applies if the black hole is unlocked)

42ef3a9f1b467cf5136758549342813425bb39f2
Omsi <omsi2000@gmail.com>
2020-04-21 00:25:52 -0700

    fix offline simulation modal number display

--- javascripts/components/modals/modal-progress-bar.js
+++ javascripts/components/modals/modal-progress-bar.js
@@ -17,7 +17,7 @@ Vue.component("modal-progress-bar", {
   <div class="l-modal-overlay c-modal-overlay">
     <div class="l-modal-progress-bar c-modal">
       <div class="c-modal-progress-bar__label"> {{progress.label}} </div>
-      <div>{{progress.current}}/{{progress.max}}</div>
+      <div>{{ formatInt(progress.current) }}/{{ formatInt(Math.floor(progress.max)) }}</div>
       <div class="l-modal-progress-bar__hbox">
         <div class="l-modal-progress-bar__bg c-modal-progress-bar__bg">
           <div class="l-modal-progress-bar__fg c-modal-progress-bar__fg" :style="foregroundStyle"/>

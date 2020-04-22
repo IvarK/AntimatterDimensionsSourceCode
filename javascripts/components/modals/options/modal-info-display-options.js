c89d2f6f20d7649a18e7fa87166dd42309e3f1d5
Dan Simon <dan@pipeline.com>
2020-04-22 08:56:28 -0400

    Fix #1274

--- javascripts/components/modals/options/modal-info-display-options.js
+++ javascripts/components/modals/options/modal-info-display-options.js
@@ -45,12 +45,12 @@ Vue.component("modal-info-display-options", {
   },
   template:
     `<modal-options @close="emitClose">
-      <on-off-button v-model="achievements" text="Achievement IDs:"/>
-      <on-off-button v-if="infinityUnlocked" v-model="challenges" text="Challenge IDs:"/>
-      <on-off-button v-if="eternityUnlocked" v-model="studies" text="Time Study IDs:"/>
-      <on-off-button v-if="realityUnlocked" v-model="realityUpgrades" text="Reality Upgrade IDs:"/>
-      <on-off-button v-if="realityUnlocked" v-model="perks" text="Perk IDs:"/>
-      <on-off-button v-if="alchemyUnlocked" v-model="alchemy" text="Alchemy resource amounts:"/>
+      <wide-on-off-button v-model="achievements" text="Achievement IDs:"/>
+      <wide-on-off-button v-if="infinityUnlocked" v-model="challenges" text="Challenge IDs:"/>
+      <wide-on-off-button v-if="eternityUnlocked" v-model="studies" text="Time Study IDs:"/>
+      <wide-on-off-button v-if="realityUnlocked" v-model="realityUpgrades" text="Reality Upgrade names:"/>
+      <wide-on-off-button v-if="realityUnlocked" v-model="perks" text="Perk IDs:"/>
+      <wide-on-off-button v-if="alchemyUnlocked" v-model="alchemy" text="Alchemy resource amounts:"/>
       Note: All types of additional info above will always display when holding shift.
     </modal-options>`
 });

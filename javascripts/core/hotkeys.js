2175dd552f4cbe330b16e11cdf3f0461073997b7
Acamaeda <acamaeda@gmail.com>
2020-07-13 21:35:28 -0400

    Added hotkeys for autobuyers
    Added hotkeys to toggle individual autobuyers. To toggle an autobuyer, press alt + the corresponding key to that action. Dimension autobuyers can also toggle buy singles with shift+alt+key.
    
    The tickspeed autobuyer can't toggle buy singles currently because I couldn't find the code that locks it to singles pre C9. The Reality autobuyer doesn't have a hotkey because Realitying doesn't have a hotkey. The RG autobuyer does instead.

--- javascripts/core/hotkeys.js
+++ javascripts/core/hotkeys.js
@@ -25,10 +25,58 @@ GameKeyboard.bindRepeatableHotkey("e", () => eternityResetRequest());
 GameKeyboard.bind("r", () => setHoldingR(true), "keydown");
 GameKeyboard.bind("r", () => setHoldingR(false), "keyup");
 
+
+const AUTOBUYER_NAMES = ["1st Dimension", "2nd Dimension", "3rd Dimension", "4th Dimension", "5th Dimension", "6th Dimension", "7th Dimension", "8th Dimension", "Tickspeed", 
+"Dimension Boost", "Galaxy", "Big Crunch", "Sacrifice", "Eternity"];
+
+// Toggle autobuyers
+function toggleAutobuyer(id) {
+  if (!player.options.autobuyersOn) {
+    return;
+  }
+  
+  const buyer = Autobuyers.all[id];
+  if (buyer.isUnlocked) {
+    buyer.toggle();
+    GameUI.notify.info(AUTOBUYER_NAMES[id] + " autobuyer " + ((buyer.isActive) ? "enabled" : "disabled"));
+  }
+}
+
+function toggleBuySingles(id) {
+  if (!player.options.autobuyersOn) {
+    return;
+  }
+  
+  const buyer = Autobuyers.all[id];
+  if (buyer.isUnlocked && buyer.toggleMode !== null) {
+    buyer.toggleMode();
+    GameUI.notify.info(AUTOBUYER_NAMES[id] + " autobuyer set to buy " + ((buyer.mode == 1) ? "singles" : "bulk"));
+  }
+}
+
+
+GameKeyboard.bindHotkey("alt+t", () => toggleAutobuyer(8));
+
+//GameKeyboard.bindHotkey("shift+alt+t", () => toggleBuySingles(8)); Disabled for now because you need to unlock buy max
+GameKeyboard.bindHotkey("alt+d", () => toggleAutobuyer(9));
+GameKeyboard.bindHotkey("alt+g", () => toggleAutobuyer(10));
+GameKeyboard.bindHotkey("alt+c", () => toggleAutobuyer(11));
+GameKeyboard.bindHotkey("alt+s", () => toggleAutobuyer(12));
+GameKeyboard.bindHotkey("alt+e", () => toggleAutobuyer(13));
+GameKeyboard.bindHotkey("alt+r", () => {
+  const buyer = Replicanti.galaxies.autobuyer;
+  if (buyer.isUnlocked) {
+    buyer.toggle();
+    GameUI.notify.info("Replicanti Galaxy autobuyer " + ((buyer.isOn) ? "enabled" : "disabled"));
+  }
+});
+
 (function() {
   function bindDimensionHotkeys(tier) {
     GameKeyboard.bindRepeatableHotkey(`${tier}`, () => buyManyDimension(tier));
     GameKeyboard.bindRepeatableHotkey(`shift+${tier}`, () => buyOneDimension(tier));
+    GameKeyboard.bindHotkey(`alt+${tier}`, () => toggleAutobuyer(tier - 1));
+    GameKeyboard.bindHotkey(`shift+alt+${tier}`, () => toggleBuySingles(tier - 1));
   }
   for (let i = 1; i < 9; i++) bindDimensionHotkeys(i);
 }());

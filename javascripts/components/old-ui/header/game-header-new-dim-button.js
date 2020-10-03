124c245dceb2cb9ded0d64d89076932e44184b41
Waiting Idly <bbenw11@gmail.com>
2020-10-03 14:43:25 -0700

    Made unlocking an Infinity Dimension move you to the ID page

--- javascripts/components/old-ui/header/game-header-new-dim-button.js
+++ javascripts/components/old-ui/header/game-header-new-dim-button.js
@@ -11,7 +11,7 @@ Vue.component("game-header-new-dim-button", {
   },
   methods: {
     update() {
-      this.isVisible = player.break && !InfinityDimension(8).isUnlocked && 
+      this.isVisible = player.break && !InfinityDimension(8).isUnlocked &&
         player.infinityPoints.lt(Player.eternityGoal);
       if (!this.isVisible) return;
       const requirement = InfinityDimensions.next().requirement;
@@ -25,7 +25,7 @@ Vue.component("game-header-new-dim-button", {
       v-if="isVisible"
       :enabled="isAffordable"
       class="o-primary-btn--new-dim l-game-header__new-dim-btn"
-      onclick="InfinityDimensions.unlockNext()"
+      onclick="InfinityDimensions.unlockNext(); Tab.dimensions.infinity.show()"
     >
       Get {{format(requirement, 0, 0)}} antimatter<br/>to unlock a new
       <span v-if="anyInfinityDimensionUnlocked">Infinity Dimension</span><span v-else>type of Dimension</span>.

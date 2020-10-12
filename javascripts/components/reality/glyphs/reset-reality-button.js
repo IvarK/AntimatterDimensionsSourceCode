6de5b16287bd2bfd16bfab6e35b262e7dc614614
Waiting Idly <bbenw11@gmail.com>
2020-10-11 23:15:07 -0700

    Made the reset reality button change its text and color.
    Added to styles.css accordingly

--- javascripts/components/reality/glyphs/reset-reality-button.js
+++ javascripts/components/reality/glyphs/reset-reality-button.js
@@ -4,22 +4,42 @@ Vue.component("reset-reality-button", {
   data() {
     return {
       canReality: false,
+      resetCelestial: false,
+      isInCelestialReality: false,
     };
+  },
+  computed: {
+    classObject() {
+      return {
+        "l-reset-reality-button": true,
+        "c-reset-reality-button": true,
+        "c-reset-reality-button-celestial": this.isInCelestialReality,
+      };
+    }
   },
   methods: {
     update() {
       this.canReality = TimeStudy.reality.isBought && player.eternityPoints.gte("1e4000");
+      this.resetCelestial = player.options.retryCelestial;
+      this.isInCelestialReality = Object.entries(player.celestials).map(x => x[1].run).includes(true);
     },
     resetReality() {
       resetReality();
     },
+    resetText() {
+      if (this.isInCelestialReality && !this.resetCelestial) {
+        return "Exit Celestial early";
+      }
+      if (this.isInCelestialReality && this.resetCelestial) {
+        return "Start this Celestial over";
+      }
+      return "Start this Reality over";
+    },
   },
   template: `
-  <button :class="['l-reset-reality-button', 'c-reset-reality-button']"
+  <button :class="classObject"
           @click="resetReality">
-    <div class="l-reality-button__contents">
-      Start this Reality over
-    </div>
+    <div class="l-reality-button__contents">{{ resetText() }}</div>
   </button>
   `
 });

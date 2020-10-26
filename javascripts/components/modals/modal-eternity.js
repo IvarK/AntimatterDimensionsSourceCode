d013351af88306a68e25a3b02d8a21e1ec8621af
Waiting Idly <bbenw11@gmail.com>
2020-10-26 07:39:52 -0700

    Touch ups, Added EC exiting to Eternity modal

--- javascripts/components/modals/modal-eternity.js
+++ javascripts/components/modals/modal-eternity.js
@@ -1,60 +1,92 @@
 "use strict";
 
 Vue.component("modal-eternity", {
-    computed: {
-        message() {
-            return `Eternity will reset everything except achievements and challenge records.
-            You will also gain an Eternity point and unlock various upgrades.`;
-        },
-        gainedEPOnEternity() {
-            return `You will gain ${format(gainedEternityPoints(), 2)} Eternity Points on Eternity.`;
-        },
-        startWithIP() {
-            return Player.startingIP.gt(0) ? `You will start your next Eternity with ${format(Player.startingIP, 2)} IP.` : ``;
-        }
+  data() {
+    return {
+      exitingEC: false,
+    };
+  },
+  computed: {
+    message() {
+      return PlayerProgress.eternityUnlocked()
+      ? `Eternity will reset everything except Achievements and challenge records.`
+      : `Eternity will reset everything except Achievements and challenge records.
+      You will also gain an Eternity Point and unlock various upgrades.`;
     },
-    methods: {
-        handleNoClick() {
-            this.emitClose();
-        },
-        handleYesClick() {
-            if (player.dilation.active && player.options.animations.dilation && document.body.style.animation === "") {
-                undilationAnimation();
-                setTimeout(eternity, 1000);
-            } else if (!player.dilation.active && player.options.animations.eternity && document.body.style.animation === "") {
-                eternityAnimation();
-                setTimeout(eternity, 2250);
-            } else {
-                eternity();
-            }
-            this.emitClose();
-        }
+    gainedEPOnEternity() {
+      return `You will gain ${format(gainedEternityPoints(), 2)} Eternity Points on Eternity.`;
     },
-    template: `
+    startWithIP() {
+      return Player.startingIP.gt(0)
+      ? `You will start your next Eternity with ${format(Player.startingIP, 2)} Infinity Points.`
+      : ``;
+    },
+    eternityChallenge() {
+      const ec = EternityChallenge.current;
+      if (ec.isFullyCompleted) {
+        return `Eternity Challenge ${ec.id} is already fully completed.`;
+      }
+      if (!Perk.studyECBulk.isBought) {
+        return `You will gain one completion of Eternity Challenge ${EternityChallenge.current.id}.`;
+      }
+
+      return `You will gain ${ec.gainedCompletionStatus.gainedCompletions}
+      ${pluralize("completion", ec.gainedCompletionStatus.gainedCompletions)} for Eternity Challenge ${ec.id}.`;
+    }
+  },
+  methods: {
+    update() {
+      this.exitingEC = EternityChallenge.isRunning;
+    },
+    handleNoClick() {
+      this.emitClose();
+    },
+    handleYesClick() {
+      if (player.dilation.active && player.options.animations.dilation && document.body.style.animation === "") {
+        undilationAnimation();
+        setTimeout(eternity, 1000);
+      } else if (!player.dilation.active && player.options.animations.eternity &&
+        document.body.style.animation === "") {
+        eternityAnimation();
+        setTimeout(eternity, 2250);
+      } else {
+        eternity();
+      }
+      this.emitClose();
+    }
+  },
+  template: `
     <div class="c-modal-message l-modal-content--centered">
-    <h2>You are about to Eternity</h2>
-    <div class="c-modal-message__text">
-    {{ message }}
-    <br>
-    </div>
-    <br>
-    <div class="c-modal-message__text">
-    {{ gainedEPOnEternity }}
-    </div>
-    <br>
-    <div class="c-modal-message__text">
-    {{ startWithIP }}
-    </div>
-    <div class="l-options-grid__row">
-    <primary-button
-            class="o-primary-btn--width-medium c-modal-message__okay-btn"
-            @click="handleNoClick"
-          >Cancel</primary-button>
-          <primary-button
-            class="o-primary-btn--width-medium c-modal-message__okay-btn"
-            @click="handleYesClick"
-          >Confirm</primary-button>
-    </div>
-    </div>
-    `
+      <div v-if="!exitingEC">
+        <h2>You are about to Eternity</h2>
+        <div class="c-modal-message__text">
+          {{ message }}
+          <br>
+        </div>
+        <br>
+        <div class="c-modal-message__text">
+          {{ gainedEPOnEternity }}
+        </div>
+        <br>
+        <div class="c-modal-message__text">
+          {{ startWithIP }}
+        </div>
+      </div>
+      <div v-else>
+        <h2>Complete Eternity Challenge</h2>
+        <div class="c-modal-message__text">
+          {{ eternityChallenge }}
+        </div>
+      </div>
+      <div class="l-options-grid__row">
+        <primary-button
+                class="o-primary-btn--width-medium c-modal-message__okay-btn"
+                @click="handleNoClick"
+              >Cancel</primary-button>
+              <primary-button
+                class="o-primary-btn--width-medium c-modal-message__okay-btn c-modal__confirm-btn"
+                @click="handleYesClick"
+              >Confirm</primary-button>
+      </div>
+    </div>`
 });

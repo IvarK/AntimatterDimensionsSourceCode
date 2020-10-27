d2a3d611ab48527b2efbc76df4182ab6d6192136
earthernsence <divine.icbm@gmail.com>
2020-10-27 13:13:54 -0500

    change reality modal as requested

--- javascripts/components/modals/modal-reality.js
+++ javascripts/components/modals/modal-reality.js
@@ -16,43 +16,14 @@ Vue.component("modal-reality", {
                 based on your Eternity Points, Replicanti, and Dilated Time, a Perk Point to spend on quality of
                 life upgrades, and unlock various upgrades.`;
         },
-        gainedRMOnReality() {
-            return `You will gain ${format(gainedRealityMachines(), 2)} Reality Machines on Reality.`;
+        gainedPPAndRealitiesAndRMOnReality() {
+            const a = `You will gain ${format(gainedRealityMachines(), 2)} Reality Machines on Reality.`;
+            const c = simulatedRealityCount();
+            const b = `You will gain ${format(gainedRealityMachines(), 2)} Reality Machines on Reality,
+            ${format(c, 2)} Perk ${pluralize("Point", c, "Points")} on Reality, 
+            and ${format(c, 2)} ${pluralize("Reality", c, "Realities")} on Reality.`;
+            return this.simulatedRealityCount() ? `${b}` : `${a}`;
         },
-        gainedPPAndRealitiesOnReality() {
-            const b = simulatedRealityCount();
-            const a = `You will gain ${format(b, 2)} Perk ${pluralize("Point", b, "Points")} on Reality, 
-            and ${format(b, 2)} ${pluralize("Reality", b, "Realities")} on Reality.`;
-            return b >= 1 ? `${a}` : ``;
-        },
-        startWithIP() {
-            return this.isPerkBought("startIP1") ? `You will start your next Reality with 
-            ${format(Player.startingIP, 2)} Infinity Points.` : ``;
-        },
-        startWithEP() {
-            return this.isPerkBought("startEP1") ? `You will start your next Reality with 
-            ${format(Player.startingEP, 2)} Eternity Points.` : ``;
-        },
-        startWithAM() {
-            return this.isPerkBought("startAM1") || this.isPerkBought("achgroup6") ? `You will start your next Reality
-            with ${format(Currency.antimatter.startingValue, 2)} Antimatter.` : ``;
-        },
-        startWithEU() {
-            if (this.isPerkBought("autounlockEU1") && !this.isPerkBought("autounlockEU2")) {
-                return `You will start your next Reality with the first row of Eternity Upgrades.`;
-            }
-            if (this.isPerkBought("autounlockEU1") && this.isPerkBought("autounlockEU2")) {
-                return `You will start your next Reality with both rows of Eternity Upgrades.`;
-            }
-            return ``;
-        },
-        startWithAch() {
-            if (!Perk.achievementGroup6.isBought) return ``;
-            if (Perk.achievementGroup6.isBought) {
-                return `You will start your next Reality with all Normal Achievements.`;
-            }
-            return ``;
-        }
     },
     methods: {
         handleNoClick() {
@@ -65,15 +36,6 @@ Vue.component("modal-reality", {
         simulatedRealityCount() {
             return simulatedRealityCount() >= 1;
         },
-        isPerkBought(perk) {
-            if (perk === "startAM1") return Perk.startAM1.isBought;
-            if (perk === "startIP1") return Perk.startIP1.isBought;
-            if (perk === "autounlockEU1") return Perk.autounlockEU1.isBought;
-            if (perk === "autounlockEU2") return Perk.autounlockEU2.isBought;
-            if (perk === "achgroup6") return Perk.achievementGroup6.isBought;
-            if (perk === "startEP1") return Perk.startEP1.isBought;
-            return `Undefined perk in method isPerkBought (${perk})`;
-        },
     },
     template: `
         <div class="c-modal-message l-modal-content--centered">
@@ -84,31 +46,9 @@ Vue.component("modal-reality", {
             </div>
         <br>
         <div class="c-modal-message__text">
-            {{ gainedRMOnReality }}
+            {{ gainedPPAndRealitiesAndRMOnReality }}
         </div>
         <br>
-         <div class="c-modal-message__text" v-if="simulatedRealityCount">
-            {{ gainedPPAndRealitiesOnReality }}
-            <br>
-        </div>
-        <div class="c-modal-message__text" v-if="isPerkBought('startAM1')">
-            {{ startWithAM }}
-            <br>
-        </div>
-        <div class="c-modal-message__text" v-if="isPerkBought('startIP1')">
-            {{ startWithIP }}
-        </div>
-        <div class="c-modal-message__text" v-if="isPerkBought('startEP1')">
-            {{ startWithEP }}
-        </div>
-        <div class="c-modal-message__text" v-if="isPerkBought('autounlockEU1')">
-            {{ startWithEU }}
-            <br>
-        </div>
-        <div class="c-modal-message__text" v-if="isPerkBought('achgroup6')">
-            {{ startWithAch }}
-            <br>
-        </div>
             <div class="l-options-grid__row">
                 <primary-button
                 class="o-primary-btn--width-medium c-modal-message__okay-btn"

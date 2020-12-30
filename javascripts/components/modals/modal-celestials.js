c19e74121149b8d5fb74f777dccc52a2d595b5ad
Waiting Idly <bbenw11@gmail.com>
2020-12-29 20:15:29 -0800

    Added case and error if Pelle is attempted

--- javascripts/components/modals/modal-celestials.js
+++ javascripts/components/modals/modal-celestials.js
@@ -51,6 +51,8 @@ Vue.component("modal-celestials", {
             case 5:
               Laitela.initializeRun();
               break;
+            case 6:
+              throw new Error(`Pelle confirmations not implemented yet.`);
             default:
               throw new Error(`Attempted to start an Unknown Celestial in Celestial Modal Confirmation.`);
           }

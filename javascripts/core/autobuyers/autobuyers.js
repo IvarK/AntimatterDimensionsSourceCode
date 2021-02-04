1b0c41aba3d3bd75b6af0a1dfc057979e765be62
Waiting Idly <bbenw11@gmail.com>
2021-02-04 14:54:50 -0800

    Reworking Autobuyers stuff

--- javascripts/core/autobuyers/autobuyers.js
+++ javascripts/core/autobuyers/autobuyers.js
@@ -1,39 +1,46 @@
 "use strict";
 
 const Autobuyers = (function() {
-  const antimatterDims = AntimatterDimensionAutobuyerState.index;
-  const infinityDims = InfinityDimensionAutobuyerState.index;
-  const timeDims = TimeDimensionAutobuyerState.index;
-  const dimensions = antimatterDims.concat(infinityDims, timeDims);
+  const antimatterDimensions = Autobuyer.antimatterDimension.index;
+  const infinityDimensions = Autobuyer.infinityDimension.index;
+  const timeDimensions = Autobuyer.timeDimension.index;
+  const dimensions = [antimatterDimensions, infinityDimensions, timeDimensions];
   const prestige = [
-    Autobuyer.bigCrunch,
-    Autobuyer.eternity,
-    Autobuyer.reality,
+    [Autobuyer.bigCrunch],
+    [Autobuyer.eternity],
+    [Autobuyer.reality],
+  ];
+  const singleBinary = [
+    [Autobuyer.replicantiGalaxy],
+    [Autobuyer.timeTheorem],
+    [Autobuyer.ipMult],
+    [Autobuyer.epMult],
   ];
   const single = [
-    Autobuyer.tickspeed,
-    Autobuyer.sacrifice,
-    Autobuyer.dimboost,
-    Autobuyer.galaxy,
-    Autobuyer.replicantiGalaxy,
-    Autobuyer.timeTheorem,
-    Autobuyer.ipMult,
-    Autobuyer.epMult,
+    [Autobuyer.tickspeed],
+    [Autobuyer.sacrifice],
+    [Autobuyer.dimboost],
+    [Autobuyer.galaxy],
+  ].concat(singleBinary);
+  const other = [
+    Autobuyer.replicantiUpgrade.index,
+    Autobuyer.dilationUpgrade.index,
+    Autobuyer.realityUpgrade.index,
+    Autobuyer.blackHolePower.index,
   ];
-  const other = BlackHolePowerAutobuyerState.index.concat(
-                RealityUpgradeAutobuyerState.index,
-                ReplicantiUpgradeAutobuyerState.index,
-                DilationUpgradeAutobuyerState.index
-    );
   const all = dimensions.concat(prestige, single, other);
 
   return {
-    all,
+    all: all.flat(),
+    display: [dimensions, other, singleBinary],
+    antimatterDimensions,
+    infinityDimensions,
+    timeDimensions,
     dimensions,
     prestige,
     single,
     other,
-    upgradeable: antimatterDims.concat([Autobuyer.tickspeed]),
+    upgradeable: antimatterDimensions.concat([Autobuyer.tickspeed]),
 
     get unlocked() {
       return Autobuyers.all.filter(a => a.isUnlocked || a.isBought);
@@ -47,11 +54,11 @@ const Autobuyers = (function() {
       PerformanceStats.start("Autobuyers");
 
       const priorityQueue = [Autobuyer.tickspeed]
-        .concat(antimatterDims)
+        .concat(antimatterDimensions)
         .sort((a, b) => a.priority - b.priority);
 
-      const autobuyers = prestige
-        .concat(single, other, infinityDims, timeDims)
+      const autobuyers = prestige.flat()
+        .concat(single.flat(), other.flat(), infinityDimensions, timeDimensions)
         .concat(priorityQueue)
         .filter(a => a.canTick);
 

02ce1152b45de5dae34cea1473cae9dca187e964
earthernsence <divine.icbm@gmail.com>
2021-03-11 19:58:34 -0600

    did some work around the office

--- javascripts/components/modals/modal-reality.js
+++ javascripts/components/modals/modal-reality.js
@@ -1,6 +1,37 @@
 "use strict";
 
+// This code is mostly just an amalgamation of glyph-peek and glyph selection. There's no new fancy ideas here at all.
+
 Vue.component("modal-reality", {
+  data() {
+    return {
+      glyphs: [],
+      canSacrifice: false,
+      levelDifference: 0,
+      selectedGlyph: Number,
+      level: 0,
+      canPeek: false,
+      isVisible: false,
+      canRefresh: false,
+      effects: GameDatabase.reality.glyphEffects,
+      grabbedEffects: [],
+      sortedEffects: [],
+    };
+  },
+  created() {
+    // This refreshes the glyphs shown after every reality, and also doesn't
+    // allow it to refresh if you're choosing glyphs (at that point,
+    // your choices are your choices). This is technically incorrect since
+    // while you're choosing glyphs the level might increase, and this code
+    // stops it from increasing in the glyphs shown here, but with
+    // the glyph choice popup open, you can't see the tooltips, so there's
+    // no way for the player to notice that.
+    this.on$(GAME_EVENT.GLYPH_CHOICES_GENERATED, () => {
+      this.canRefresh = false;
+    });
+    this.on$(GAME_EVENT.REALITY_RESET_AFTER, this.refreshGlyphs);
+    this.getGlyphs();
+  },
   computed: {
     message() {
       return `Reality will reset everything except challenge records. Your Achievements are also reset, 
@@ -18,15 +49,67 @@ Vue.component("modal-reality", {
     },
     can() {
       return isRealityAvailable();
+    },
+    direction() {
+      if (this.glyphs[0].level > player.records.bestReality.glyphLevel) return "higher";
+      return "lower";
     }
   },
   methods: {
+    getGlyphs() {
+      this.canRefresh = true;
+      this.glyphs = GlyphSelection.glyphList(
+        GlyphSelection.choiceCount, gainedGlyphLevel(), { isChoosingGlyph: false });
+      this.level = gainedGlyphLevel().actualLevel;
+      this.grabEffects();
+    },
     handleYesClick() {
       requestManualReality();
+      GlyphSelection.select(this.selectedGlyph, false);
       this.emitClose();
     },
     handleNoClick() {
+      console.log(this.glyphs);
+      console.log(this.grabbedEffects);
       this.emitClose();
+    },
+    update() {
+      if (!GlyphSelection.glyphs.length) return;
+      for (let i = 0; i < this.glyphs.length; ++i) {
+        const currentGlyph = this.glyphs[i];
+        const newGlyph = GlyphSelection.glyphs[i];
+        if (currentGlyph.level === newGlyph.level) continue;
+        currentGlyph.level = newGlyph.level;
+        currentGlyph.effects = newGlyph.effects;
+      }
+      this.canSacrifice = RealityUpgrade(19).isEffectActive;
+      this.levelDifference = Math.abs(player.records.bestReality.glyphLevel - this.glyphs[0].level);
+      
+      // Hide this before first reality since then it'll confuse the player,
+      // and due to pre-selected first glyph might well be incorrect anyway.
+      this.isVisible = player.realities > 0 && TimeStudy.reality.isBought;
+      this.canPeek = player.realities > 0;
+    },
+    select(index) {
+      this.selectedGlyph = index;
+
+    },
+    trashGlyphs() {
+      if (!player.options.confirmations.glyphSacrifice ||
+        confirm("Are you sure you want to sacrifice a random one of these glyphs?")) {
+        GlyphSelection.select(Math.floor(Math.random() * GlyphSelection.choiceCount), true);
+      }
+    },
+    grabEffects() {
+      if (!this.glyphs.length >= 4) return;
+      for (let i = 0; i < this.glyphs.length; i++) {
+        console.log(this.glyphs[i]);
+        console.log(this.level);
+          this.grabbedEffects.push(getGlyphEffectValuesFromBitmask(this.glyphs[i].effects, this.level, this.glyphs[i].strength)
+          // eslint-disable-next-line no-loop-func
+          .filter(effect => GameDatabase.reality.glyphEffects[effect.id].isGenerated === generatedTypes.includes(this.glyphs[i].type)));
+      } 
+      console.log(this.grabbedEffects);
     }
   },
   template: 
@@ -39,6 +122,31 @@ Vue.component("modal-reality", {
       <div class="c-modal-message__text">
         {{ gained }}
       </div>
+      <div class="l-modal-glyph-selection__row">
+        <glyph-component v-for="(glyph, index) in glyphs"
+                        class="l-modal-glyph-selection__glyph"
+                        :key="index"
+                        :glyph="glyph"
+                        :noLevelOverride="true"
+                        :showSacrifice="canSacrifice"
+                        @click.native="select(index)"
+                        />
+      </div>
+      <button class="o-primary-btn o-primary-btn--glyph-trash"
+        v-if="canSacrifice"
+        v-on:click="trashGlyphs()">
+          I don't want any of these Glyphs,
+          <br>
+          pick and Sacrifice one at random.
+          <br>
+          (these are {{ formatInt(levelDifference) }} {{"level" | pluralize(levelDifference)}}
+          {{ direction }} than your best)
+      </button>
+      <div v-for="object in grabbedEffects">
+        <div v-for="effect in object">
+          {{  }}: {{  }}
+        </div>
+      </div>
         <div class="l-options-grid__row">
           <primary-button
           class="o-primary-btn--width-medium c-modal-message__okay-btn"
@@ -46,7 +154,7 @@ Vue.component("modal-reality", {
           >Cancel</primary-button>
           <primary-button
           class="o-primary-btn--width-medium c-modal-message__okay-btn c-modal__confirm-btn"
-          @click="handleYesClick"
+          @click.native="handleYesClick"
           :enabled="can"
           >Reality</primary-button>
         </div>

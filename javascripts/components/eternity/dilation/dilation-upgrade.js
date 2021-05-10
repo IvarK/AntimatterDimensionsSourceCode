44dfb28bb29171700b495d0d155513e3e830af96
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2021-05-10 11:54:30 -0500

    Improve dilation upgrade tooltips

--- javascripts/components/eternity/dilation/dilation-upgrade.js
+++ javascripts/components/eternity/dilation/dilation-upgrade.js
@@ -35,16 +35,17 @@ Vue.component("dilation-upgrade", {
       };
     },
     timeEstimate() {
-      // Don't show if less than 10 seconds or more than a year
-      return this.timeUntilCost.lt(10) || this.timeUntilCost.gt(86400 * 365.25) 
-        ? null
-        : TimeSpan.fromSeconds(this.timeUntilCost.toNumber()).toStringShort(false);
+      if (this.isAffordable || this.upgrade.isBought) return null;
+      if (this.timeUntilCost.lt(1)) return `< ${formatInt(1)} second`;
+      if (this.timeUntilCost.gt(86400 * 365.25)) return `> ${formatInt(1)} year`;
+      return TimeSpan.fromSeconds(this.timeUntilCost.toNumber()).toStringShort(false);
     }
   },
   methods: {
     update() {
       const upgrade = this.upgrade;
-      this.timeUntilCost = Decimal.sub(upgrade.cost, Currency.dilatedTime.value).div(getDilationGainPerSecond());
+      this.timeUntilCost = Decimal.sub(upgrade.cost, Currency.dilatedTime.value)
+        .div(getDilationGainPerSecond().times(getGameSpeedupForDisplay()));
       if (this.isRebuyable) {
         this.isAffordable = upgrade.isAffordable;
         this.isCapped = upgrade.isCapped;

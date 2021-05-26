3ea2d0300f7780704de6a34659ebdeb5acda4988
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2021-05-26 18:52:29 -0500

    why is UI difficult

--- javascripts/components/modals/options/modal-hidden-tabs.js
+++ javascripts/components/modals/options/modal-hidden-tabs.js
@@ -12,25 +12,21 @@ Vue.component("modal-hidden-tabs", {
     },
   },
   template: `
-  <div class="c-modal-message l-modal-content--centered">
+  <div class="l-hide-tab-modal">
   <modal-close-button @click="emitClose"/>
-  Click a button to toggle showing a tab on/off.
-  <br>
-  Some tabs cannot be hidden.
-  <br>
-    <div style="display: flex; flex-direction: column; align-items: center;">
-      <div v-for="tab in tabs">
-        <subtab-group
-          :tab="tab"/>
-        <br>
-      </div>
+    Click a button to toggle showing a tab on/off.
+    <br>
+    Some tabs cannot be hidden.
+    <br>
+    <div v-for="tab in tabs" class="l-hide-modal-tab-container">
+      <tab-modal-subtab-group :tab="tab"/>
+      <br>
     </div>
-  <br>
   </div>
   `
 });
 
-Vue.component("subtab-group", {
+Vue.component("tab-modal-subtab-group", {
   props: {
     tab: Object,
   },
@@ -38,38 +34,83 @@ Vue.component("subtab-group", {
     return {
       tabName: String,
       subtabs: Object,
+      hidden: Boolean,
     };
   },
   computed: {
+    classObject() {
+      return {
+        "l-hide-modal-tab-button": true,
+        "c-hide-modal-button--active": !this.hidden,
+        "c-hide-modal-button--inactive": this.hidden,
+        "c-hide-modal-button--always-visible": !this.tab.config.hidable,
+      };
+    },
   },
   methods: {
     update() {
       this.tabName = this.tab.config.name;
       this.subtabs = this.tab.subtabs;
+      this.hidden = this.tab.isHidden && this.tab.config.hidable;
     },
     toggleVisibility(tab) {
       tab.toggleVisibility();
     },
-    classObject(subtab) {
+    getKey(subtab) {
+      // eslint-disable-next-line no-bitwise
+      return `${1 << (this.tab.config.id)} ${1 << (subtab.config.id + 1)} ${subtab.isHidden}`;
+    }
+  },
+  template: `
+    <div class="c-hide-modal-all-subtab-container l-hide-modal-subtab-container"
+      v-if="tab.isUnlocked">
+        <div :class="classObject"
+          @click="toggleVisibility(tab)">
+            {{ tabName }}
+            <br>
+            (hide main tab)
+        </div>
+        <br>
+        <tab-modal-subtab-button
+          v-for="subtab in subtabs"
+          v-if="subtab.isUnlocked"
+          :key="getKey(subtab)"
+          :subtab="subtab"/>
+    </div>
+  `
+});
+
+Vue.component("tab-modal-subtab-button", {
+  props: {
+    subtab: Object,
+  },
+  data() {
+    return {
+      hidden: Boolean,
+    };
+  },
+  computed: {
+    classObject() {
       return {
-        "o-tab-btn": true,
-        "o-tab-btn--secondary": true,
-        "o-primary-btn--disabled": subtab.isHidden && subtab.hidable,
+        "l-hide-modal-subtab-button": true,
+        "c-hide-modal-button--active": !this.hidden,
+        "c-hide-modal-button--inactive": this.hidden,
+        "c-hide-modal-button--always-visible": !this.subtab.config.hidable,
       };
     },
   },
+  methods: {
+    update() {
+      this.hidden = this.subtab.isHidden && this.subtab.config.hidable;
+    },
+    toggleVisibility() {
+      this.subtab.toggleVisibility();
+    },
+  },
   template: `
-    <div class="c-auto-sac-type-tab__effect-desc l-specified-effect-tab__effect-desc"
-      v-if="tab.isUnlocked">
-        <div @click="toggleVisibility(tab)">
-          {{ tabName }} Tab
-        </div>
-        <br>
-        <div v-for="subtab in subtabs" v-if="subtab.isUnlocked"
-          :class="classObject(subtab)"
-          @click="toggleVisibility(subtab)">
-            {{ subtab.name }}
-        </div>
+    <div :class="classObject"
+      @click="toggleVisibility()">
+        {{ subtab.name }}
     </div>
   `
 });

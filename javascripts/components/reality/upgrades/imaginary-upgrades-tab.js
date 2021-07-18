0291d0256d0cfb6f74252367bde098a14cf7d667
WaitingIdly <bbenw11@gmail.com>
2021-07-18 14:32:42 -0700

    Explain colors and unlock of iUs (same as RUPGs)

--- javascripts/components/reality/upgrades/imaginary-upgrades-tab.js
+++ javascripts/components/reality/upgrades/imaginary-upgrades-tab.js
@@ -34,6 +34,18 @@ Vue.component("imaginary-upgrades-tab", {
       The amount of iM below the cap will be cut in half every {{ formatInt(scaleTime) }} seconds.
       <br>
       <br>
+      The first two rows of upgrades can be purchased endlessly, and the rest of the upgrades are one-time upgrades
+      which have an unlocking requirement in addition to costing Imaginary Machines.
+      <br>
+      These requirements, once completed, permanently unlock the ability to purchase the upgrades at any point.
+      <br>
+      Red upgrades are impossible to unlock in your current Reality, while yellow upgrades are still possible.
+      <br>
+      Locked upgrades show their requirement and effect by default; unlocked ones show
+      their effect, current bonus, and cost.
+      <br>
+      Hold shift to swap this behavior.
+      <br><br>
       <div v-for="row in 5" class="l-reality-upgrade-grid__row">
         <imaginary-upgrade-button
           v-for="column in 5"

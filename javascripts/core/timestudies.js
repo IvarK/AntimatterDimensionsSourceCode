83a4b86c2cf8441ede3cdddf87403ec3adeff42d
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2021-08-02 21:48:15 -0500

    Add modal explaining RM softcap at e6k EP on first reality, closes #1662

--- javascripts/core/timestudies.js
+++ javascripts/core/timestudies.js
@@ -493,6 +493,12 @@ class DilationTimeStudyState extends TimeStudyState {
     if (this.isBought || !this.canBeBought) return false;
     if (this.id === 1) unlockDilation(quiet);
     if (this.id === 6 && !Perk.autounlockReality.isBought) {
+      if (Currency.realities.eq(0)) {
+        Modal.message.show(`Reality Machine gain for your first Reality is reduced above ${format("1e6000")} EP and
+          capped at ${format("1e8000")} EP. This is due to balance changes made in the Reality update which affect
+          the difficulty of reaching those amounts of EP, such as increased Time Dimension cost scaling above
+          ${format("1e6000")} EP.`);
+      }
       Tab.reality.glyphs.show();
     }
     player.dilation.studies.push(this.id);

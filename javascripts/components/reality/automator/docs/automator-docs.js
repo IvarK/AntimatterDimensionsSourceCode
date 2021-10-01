303a29f55315f69abe32172777498be0a5be9fe7
Dan Simon <des480@nyu.edu>
2021-10-01 13:50:41 -0400

    Wrote working save compression code, not enough comments though

--- javascripts/components/reality/automator/docs/automator-docs.js
+++ javascripts/components/reality/automator/docs/automator-docs.js
@@ -102,7 +102,7 @@ Vue.component("automator-docs", {
       } else {
         // Append the script name into the beginning of the string as "name_length||name||"
         const name = AutomatorData.currentScriptName();
-        copyToClipboard(btoa(`${name.length}||${name}||${trimmed}`));
+        copyToClipboard(GameSaveSerializer.encodeText(`${name.length}||${name}||${trimmed}`));
         GameUI.notify.info("Exported current Automator script to your clipboard");
       }
     },
--- javascripts/core/storage/serializer.js
+++ javascripts/core/storage/serializer.js
@@ -3,7 +3,7 @@
 const GameSaveSerializer = {
   serialize(save) {
     const json = JSON.stringify(save, this.jsonConverter);
-    return btoa(json);
+    return this.encodeText(json);
   },
   // eslint-disable-next-line no-unused-vars
   jsonConverter(key, value) {
@@ -18,11 +18,44 @@ const GameSaveSerializer = {
   deserialize(data) {
     if (typeof data !== "string") return undefined;
     try {
-      const json = atob(data);
+      const json = this.decodeText(data);
       // eslint-disable-next-line no-unused-vars
       return JSON.parse(json, (k, v) => ((v === Infinity) ? "Infinity" : v));
     } catch (e) {
       return undefined;
+    }
+  },
+  encoder: new TextEncoder(),
+  decoder: new TextDecoder(),
+  
+  // It's important that `this` is what it should be in these function calls
+  // (encoder/decoded for the first element, window for the fourth)
+  // which is why we shouldn't do e.g. {'encode': encoder.encode, 'decode': encoder.decode}
+  // In the fifth element, order of operations is important: we don't want to encode 0s we added in encoding
+  // (i.e. + -> 0b -> 0ab is undesired) or to accidentally decode 0ac -> 0c -> / (slash)
+  // when encoding says (as it should) 0c -> 0ac.
+  steps: [
+    {'encode': x => GameSaveSerializer.encoder.encode(x), 'decode': x => GameSaveSerializer.decoder.decode(x)},
+    {'encode': x => pako.deflate(x), 'decode': x => pako.inflate(x)},
+    {
+      'encode': x => Array.from(x).map(i => String.fromCharCode(i)).join(''),
+      'decode': x => Uint8Array.from(Array.from(x).map(i => i.charCodeAt(0)))
+    },
+    {'encode': x => btoa(x), 'decode': x => atob(x)},
+    {
+      'encode': x => x.replace(/0/g, '0a').replace(/\+/g, '0b').replace(/\//g, '0c'),
+      'decode': x => x.replace(/0b/g, '+').replace(/0c/g, '/').replace(/0a/g, '0')
+    },
+    {'encode': x => 'Pa' + x, 'decode': x => x.slice(2)}
+  ],
+  encodeText(text) {
+    return this.steps.reduce((x, step) => step.encode(x), text);
+  },
+  decodeText (text) {
+    if (text.startsWith('Pa')) {
+      return this.steps.reduceRight((x, step) => step.decode(x), text);
+    } else {
+      return atob(text);
     }
   }
 };

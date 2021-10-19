601252487c147ee0ae9019c83f8077d4c82522b6
IvarK <ivarkera@gmail.com>
2021-10-19 21:23:06 +0300

    fix black hole tab description while in enslaved #1956

--- javascripts/components/reality/black-hole/black-hole-tab.js
+++ javascripts/components/reality/black-hole/black-hole-tab.js
@@ -144,7 +144,7 @@ Vue.component("black-hole-tab", {
   template: `
     <div class="l-black-hole-tab">
       <div v-if="isEnslaved">
-        The physics of this Reality do not permit singularities.
+        The physics of this Reality disables Black Hole.
       </div>
       <div v-else-if="!isUnlocked" style="display: flex; flex-direction: column; align-items: center;">
         <black-hole-unlock-button @blackholeunlock="startAnimation" />

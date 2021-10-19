56ce53487c35686772359f66e037a264bfe961e8
Iker <ikerstream@gmail.com>
2021-10-19 18:53:55 +0200

    Add docs to custom effects

--- javascripts/core/game-mechanics/custom-effect.js
+++ javascripts/core/game-mechanics/custom-effect.js
@@ -1,7 +1,12 @@
 "use strict";
-
 class CustomEffect extends Effect {
   // eslint-disable-next-line max-params
+   /**
+   * @param {String} name - The name of the created Effect
+   * @param {effectValueCallback, Number, Decimal} effect - Callback used to calculate the effect's value or constant numeric effect value
+   * @param {effectValueCallback} condition - Callback used to check whether the effect can be applied
+   * @param {effectValueCallback} cap - Callback used to calculate the effect's maximum value or constant numeric maximum value
+   */
   constructor(name, effect, condition, cap) {
     super(effect, condition, cap);
     if (!name) {

cf87e740804d6c4791abf88f549162fddb4a36b2
Dan Simon <des480@nyu.edu>
2021-11-07 21:25:04 -0500

    Stop break inf passive IP from showing when effect is 0, fix codefactor issues

--- javascripts/components/infinity/infinity-upgrade-button.js
+++ javascripts/components/infinity/infinity-upgrade-button.js
@@ -48,8 +48,12 @@ Vue.component("infinity-upgrade-button", {
       // A bit hacky, but the offline passive IP upgrade (the one that doesn't work online)
       // should hide its effect value if offline progress is disabled, in order to be
       // consistent with the other offline progress upgrades which hide as well.
-      // (Other upgrades, including rebuyables, should never hide their effect).
-      this.isDisabled = upgrade.config.isDisabled && upgrade.config.isDisabled();
+      // Also, the IP upgrade that works both online and offline should not
+      // show 0 if its value is 0. This is a bit inconvenient because sometimes,
+      // like after eternity, it can be bought but have value 0, but not showing the effect
+      // in this case doesn't feel too bad. Other upgrades, including the cost scaling
+      // rebuyables, should never hide their effect.
+      this.isDisabled = upgrade.config.isDisabled && upgrade.config.isDisabled(upgrade.config.effect());
     }
   },
   template: `

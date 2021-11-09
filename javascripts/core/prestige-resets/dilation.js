e8aeb8ef55f8de81944a8fa8e1236050cf7d470d
WaitingIdly <bbenw11@gmail.com>
2021-11-09 01:33:18 -0800

    Combine enter and exit dilation into one file

--- javascripts/core/prestige-resets/dilation.js
+++ javascripts/core/prestige-resets/dilation.js
@@ -1,5 +1,51 @@
 "use strict";
 
+class EnterDilationReset extends BaseEternityReset {
+  get confirmationOption() {
+    return player.options.confirmations.dilation;
+  }
+
+  set confirmationOption(value) {
+    player.options.confirmations.dilation = value;
+  }
+
+  confirmation(props) {
+    Modal.enterDilation.show(props);
+  }
+
+  get animationOption() {
+    return player.options.animations.dilation;
+  }
+
+  set animationOption(value) {
+    player.options.animations.dilation = value;
+  }
+
+  animation() {
+    document.body.style.animation = "dilate 2s 1 linear";
+    setTimeout(() => {
+      document.body.style.animation = "";
+    }, 2000);
+  }
+
+  // eslint-disable-next-line no-empty-function
+  tabChange() { }
+
+  gain() {
+    Achievement(136).unlock();
+    player.dilation.active = true;
+  }
+
+  reset() {
+    Reset.eternity.request({ force: true });
+    player.challenge.eternity.current = 0;
+  }
+
+  get canBePerformed() {
+    return PlayerProgress.dilationUnlocked();
+  }
+}
+
 class ExitDilationReset extends BaseEternityReset {
   get confirmationOption() {
     return player.options.confirmations.dilation;
@@ -46,5 +92,7 @@ class ExitDilationReset extends BaseEternityReset {
     return player.dilation.active;
   }
 }
+
+Reset.enterDilation = new EnterDilationReset();
 
 Reset.exitDilation = new ExitDilationReset();

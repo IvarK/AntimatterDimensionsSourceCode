d77b1f0fbe6813fe4f79b7ad092e43707763be17
earth <divine.icbm@gmail.com>
2021-12-11 15:37:48 -0600

    add indentation

--- javascripts/components/new-ui/dimensions-tab/new-tickspeed-row.js
+++ javascripts/components/new-ui/dimensions-tab/new-tickspeed-row.js
@@ -65,7 +65,7 @@ Vue.component("new-tickspeed-row", {
   template: `
     <div :class="classObject">
       <div class="tickspeed-labels">
-      <span>{{ tickspeedDisplay }} <span>{{ multiplierDisplay }}</span>
+        <span>{{ tickspeedDisplay }} <span>{{ multiplierDisplay }}</span>
         <game-header-gamespeed-display v-if="!isGameSpeedNormal" /></span>
       </div>
       <div class="tickspeed-buttons">

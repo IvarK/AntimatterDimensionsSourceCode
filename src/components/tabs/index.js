edad2a510ad0268013163b424a77fbf34d0d2ee2
Andrei Andreev <Razenpok@outlook.com>
2021-12-11 23:15:11 +0300

    Vuefy and cleanup StatisticsTab

---
+++ src/components/tabs/index.js
@@ -0,0 +1,8 @@
+// Temporarily aggregate tab components here until GameUI is migrated to SFC
+import StatisticsTab from "./statistics/StatisticsTab";
+
+const TabComponents = {
+  StatisticsTab
+};
+
+export default TabComponents;

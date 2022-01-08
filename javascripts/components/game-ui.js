671e608cfecfcc1a77eefd34f7f0b89665c11768
Hirame1 <56225774+Hirame1@users.noreply.github.com>
2022-01-08 22:17:03 +0700

    Add secret theme S11

--- javascripts/components/game-ui.js
+++ javascripts/components/game-ui.js
@@ -4,6 +4,7 @@ import "./save-timer.js";
 import "./help-me.js";
 import "./tt-shop.js";
 import "./new-ui/sidebar.js";
+import "./blob-snow.js";
 import TabComponents from "@/components/tabs";
 import PopupModal from "@/components/modals/PopupModal";
 
@@ -45,6 +46,7 @@ Vue.component("game-ui", {
         <modal-progress-bar v-if="view.modal.progressBar" />
         <link v-if="view.theme !== 'Normal'" type="text/css" rel="stylesheet" :href="themeCss">
         <help-me />
+        <blob-snow />
       </div>
       <div id="notification-container" class="l-notification-container" />
       <tt-shop v-if="view.subtab === 'studies'" class="l-time-studies-tab__tt-shop" />

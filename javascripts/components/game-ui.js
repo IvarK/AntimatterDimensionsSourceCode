ae2327f8d4d547929d1b54d51f78adaa30ccb2c4
Hirame1 <56225774+Hirame1@users.noreply.github.com>
2022-01-10 23:31:07 +0700

    Change animation style to snowfall type again

--- javascripts/components/game-ui.js
+++ javascripts/components/game-ui.js
@@ -5,7 +5,7 @@ import "./speedrun-status.js";
 import "./help-me.js";
 import "./tt-shop.js";
 import "./new-ui/sidebar.js";
-import "./blob-particles";
+import "./blob-snowflakes";
 import TabComponents from "@/components/tabs";
 import PopupModal from "@/components/modals/PopupModal";
 
@@ -57,7 +57,7 @@ Vue.component("game-ui", {
         <modal-progress-bar v-if="view.modal.progressBar" />
         <link v-if="view.theme !== 'Normal'" type="text/css" rel="stylesheet" :href="themeCss">
         <help-me />
-        <blob-particles v-if="animateBlobBackground" />
+        <blob-snowflakes v-if="animateBlobBackground" />
       </div>
       <div id="notification-container" class="l-notification-container" />
       <tt-shop v-if="view.subtab === 'studies'" class="l-time-studies-tab__tt-shop" />

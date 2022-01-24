fd488be573264c084d1d0ab646ba34716cf33019
IvarK <ivarkera@gmail.com>
2022-01-25 01:37:19 +0200

    add blobyon particles

--- javascripts/components/eternity/dilation/tachyon-particles.js
+++ javascripts/components/eternity/dilation/tachyon-particles.js
@@ -3,6 +3,7 @@ Vue.component("tachyon-particles", {
     return {
       count: 0,
       initialized: false,
+      isBlobs: false,
       bounds: {
         x: 0,
         y: 0
@@ -19,6 +20,7 @@ Vue.component("tachyon-particles", {
   },
   methods: {
     update() {
+      this.isBlobs = Theme.current().name === "S11";
       this.count = Currency.tachyonParticles.gte(1)
         ? Math.clampMin(Math.floor(20 * Math.log10(Currency.tachyonParticles.exponent)), 1)
         : 0;
@@ -30,7 +32,8 @@ Vue.component("tachyon-particles", {
   },
   template:
     `<svg class="c-tachyon-particle-container">
-      <tachyon-particle
+      <component
+        :is="isBlobs ? 'tachyon-blob' : 'tachyon-particle'"
         v-for="i in count"
         v-if="initialized"
         :key="i"
@@ -39,7 +42,7 @@ Vue.component("tachyon-particles", {
     </svg>`
 });
 
-Vue.component("tachyon-particle", {
+Vue.component("tachyon-particle", { 
   props: {
     bounds: Object
   },
@@ -100,5 +103,77 @@ Vue.component("tachyon-particle", {
     }
   },
   template:
-    `<circle r="2" class="o-tachyon-particle" />`
+    `<circle r="2" class="o-tachyon-particle"/>`
+});
+
+
+Vue.component("tachyon-blob", {
+  data() {
+    return {
+      blob: ''
+    }
+  },  
+  props: {
+    bounds: Object
+  },
+  mounted() {
+    const blobs = "";
+    this.blob = blobs[Math.floor(Math.random() * blobs.length)];
+    this.fly();
+  },
+  beforeDestroy() {
+    TWEEN.remove(this.tween);
+  },
+  methods: {
+    fly() {
+      const bounds = this.bounds;
+      const start = {
+        x: Math.random() * bounds.x,
+        y: Math.random() * bounds.y
+      };
+      const direction = randomUnitVector();
+      const MIN_SPEED = 0.5;
+      const MAX_SPEED = 1;
+      const speed = MIN_SPEED + Math.random() * (MAX_SPEED - MIN_SPEED);
+      const intersectionLength = intersect(start, direction, { x: 0, y: 0 }, bounds);
+      const intersection = {
+        x: start.x + direction.x * intersectionLength,
+        y: start.y + direction.y * intersectionLength
+      };
+      const duration = intersectionLength / speed;
+
+      const position = start;
+      this.tween = new TWEEN.Tween(position)
+        .to(intersection, duration)
+        .onUpdate(() => {
+          this.$el.setAttribute("x", position.x);
+          this.$el.setAttribute("y", position.y);
+        })
+        .easing(TWEEN.Easing.Linear.None)
+        .onComplete(this.fly.bind(this))
+        .start(tweenTime);
+
+      function randomUnitVector() {
+        const azimuth = Math.random() * 2 * Math.PI;
+        return {
+          x: Math.cos(azimuth),
+          y: Math.sin(azimuth)
+        };
+      }
+      // eslint-disable-next-line max-params
+      function intersect(rayStart, rayUnit, rectAA, rectBB) {
+        const dirfrac = {
+          x: 1 / rayUnit.x,
+          y: 1 / rayUnit.y
+        };
+        const t1 = (rectAA.x - rayStart.x) * dirfrac.x;
+        const t2 = (rectBB.x - rayStart.x) * dirfrac.x;
+        const t3 = (rectAA.y - rayStart.y) * dirfrac.y;
+        const t4 = (rectBB.y - rayStart.y) * dirfrac.y;
+        return Math.min(Math.max(t1, t2), Math.max(t3, t4));
+      }
+    }
+  },
+  template:
+  `<text class="o-tachyon-particle">{{ blob }}</text>`
 });

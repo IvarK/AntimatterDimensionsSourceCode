c587cd989897339f430f410cf62407e9e89ea8c5
Andrei Andreev <Razenpok@outlook.com>
2022-02-06 21:10:48 +0300

    Migrate codemirror to npm package

--- src/shims.js
+++ src/shims.js
@@ -3,3 +3,11 @@ window.Vue = Vue;
 
 import Decimal from "break_infinity.js";
 window.Decimal = Decimal;
+
+import CodeMirror from "codemirror/lib/codemirror";
+import "codemirror/addon/mode/simple";
+import "codemirror/addon/hint/show-hint";
+import "codemirror/addon/lint/lint";
+import "codemirror/addon/selection/active-line";
+import "codemirror/addon/edit/closebrackets";
+window.CodeMirror = CodeMirror;

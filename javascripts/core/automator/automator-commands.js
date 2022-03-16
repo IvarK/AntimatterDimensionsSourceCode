d2d1734ba3824ef858a5d4f33069a86d8312ad06
IvarK <ivar@vertics.co>
2022-03-16 15:57:04 +0200

    fix auto infinity command conversion to blocktomato, fixes #1889 (the last comment in that issue is no longer an issue)

--- javascripts/core/automator/automator-commands.js
+++ javascripts/core/automator/automator-commands.js
@@ -190,8 +190,8 @@ export const AutomatorCommands = ((() => {
           : undefined;
         const xHighest = ctx.xHighest ? ctx.xHighest[0].children.$value : undefined;
         const fixedAmount = ctx.currencyAmount
-          ? `${ctx.currencyAmount[0].children.NumberLiteral[0].image}
-            ${ctx.currencyAmount[0].children.AutomatorCurrency[0].image}`
+          ? `${ctx.currencyAmount[0].children.NumberLiteral[0].image}` +
+            ` ${ctx.currencyAmount[0].children.AutomatorCurrency[0].image}`
           : undefined;
         const on = Boolean(ctx.On);
         let input = "";

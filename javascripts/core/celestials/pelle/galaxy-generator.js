b73ee6def32948016d0e91afe88f080fd9dbb2f0
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2022-03-17 18:02:30 +0800

    Increase galaxy gen softlock penalty

--- javascripts/core/celestials/pelle/galaxy-generator.js
+++ javascripts/core/celestials/pelle/galaxy-generator.js
@@ -24,7 +24,7 @@ export const GalaxyGenerator = {
   get gainPerSecond() {
     if (!Pelle.hasGalaxyGenerator) return 0;
     // Pretend it's here to avoid softlocks and not because the bottom code returns 1 when you don't have this upg
-    if (!GalaxyGeneratorUpgrades.additive.canBeApplied) return 0.5;
+    if (!GalaxyGeneratorUpgrades.additive.canBeApplied) return 0.1;
     return DC.D1.timesEffectsOf(
       GalaxyGeneratorUpgrades.additive,
       GalaxyGeneratorUpgrades.multiplicative,

1c401c2fa6a806ff97bf74f42dd24502aab10aec
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2022-03-23 18:52:32 +0800

    centre the other cel6 icon

--- javascripts/core/secret-formula/celestials/navigation.js
+++ javascripts/core/secret-formula/celestials/navigation.js
@@ -1300,7 +1300,7 @@ GameDatabase.celestials.navigation = (function() {
         incompleteClass: "c-celestial-nav__test-incomplete",
         symbol: "ᛝ",
         symbolScale: 1.6,
-        symbolOffset: "0.3",
+        symbolOffset: "0.6",
         fill: "white",
         position: Positions.laitelaFirstCenter,
         ring: {
@@ -1623,7 +1623,7 @@ GameDatabase.celestials.navigation = (function() {
         incompleteClass: "c-celestial-nav__test-incomplete",
         symbol: "ᛝ",
         symbolScale: 1.6,
-        symbolOffset: "0.1",
+        symbolOffset: "0.6",
         fill: "white",
         position: Positions.laitelaThirdCenter,
         ring: {

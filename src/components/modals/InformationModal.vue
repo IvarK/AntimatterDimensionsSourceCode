88049ca7a20c866559cec4ee16821e6ea4b99e72
earth <divine.icbm@gmail.com>
2022-03-23 18:18:28 -0500

    improve info modal

--- src/components/modals/InformationModal.vue
+++ src/components/modals/InformationModal.vue
@@ -18,40 +18,43 @@ export default {
       </div>
     </div>
     <div class="info-body">
-      Antimatter Dimensions is an Idle Incremental game with multiple layers of unlocks, prestige, and achievements.
+      Antimatter Dimensions is an Idle Incremental game created by Finnish developer Hevipelle. Originating as a solo
+      project in 2016, it was expanded upon by a large team of developers and testers from then on.
       <br>
       <br>
-      The basic goal is to reach Infinity and receive an Infinity Point, which can be spent on various upgrades to
-      increase your overall production.
+      The game has unfolding gameplay and multiple prestige layers. The "How to Play" button contains useful
+      information about progressing.
     </div>
     <div class="socials">
       <span ach-tooltip="Antimatter Dimensions Discord Server">
         <a
           href="https://discord.gg/ST9NaXa"
           target="_blank"
-          class="socials--discord"
         ><i class="fa-brands fa-discord" /></a>
       </span>
       <span ach-tooltip="r/AntimatterDimensions">
         <a
           href="https://www.reddit.com/r/antimatterdimensions"
           target="_blank"
-          class="socials--reddit"
-        ><i class="fa-brands fa-reddit" /></a>
+        ><i class="fa-brands fa-reddit-alien" /></a>
       </span>
       <span ach-tooltip="Antimatter Dimensions on Google Play">
         <a
           href="https://play.google.com/store/apps/details?id=kajfosz.antimatterdimensions"
           target="_blank"
-          class="socials--android"
         ><i class="fa-brands fa-google-play" /></a>
       </span>
       <span ach-tooltip="Game changelog">
         <a
           href="changelog.html"
           target="_blank"
-          class="socials--changelog"
         ><i class="fa-solid fa-arrows-rotate" /></a>
+      </span>
+      <span ach-tooltip="Credits">
+        <a
+          href="https://example.com"
+          target="_blank"
+        ><i class="fa-solid fa-users-line" /></a>
       </span>
     </div>
   </div>
@@ -63,26 +66,12 @@ export default {
   justify-content: space-evenly;
   align-items: center;
   font-size: 7.5rem;
-}
-
-.socials--discord {
-  color: #5865f2;
-}
-
-.socials--reddit {
-  color: #ff4500;
-}
-
-.socials--android {
-  color: #00ceff;
-}
-
-.socials--changelog {
   color: white;
 }
 
 [ach-tooltip]:before {
-  width: 18rem;
+  width: 20rem;
+  font-size: 1.5rem;
 }
 
 .information-modal {
@@ -91,13 +80,14 @@ export default {
   margin: 0.5rem;
   width: calc(100vw - 50vh);
   height: calc(100vh - 60vh);
+  justify-content: space-around;
 }
 
 .info-body {
   margin: 1rem;
   padding: 0.5rem;
   text-align: left;
-  font-size: 1.6rem;
+  font-size: 2rem;
 }
 
 a {

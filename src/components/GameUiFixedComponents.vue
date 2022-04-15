74ec4cc4ef536ee73b66b04ec6da84a0fb3d31ef
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2022-04-15 23:42:31 +0800

    Recapitalise GameUiFixedComponents

---
+++ src/components/GameUiFixedComponents.vue
@@ -0,0 +1,83 @@
+<script>
+import HowToPlay from "@/components/HowToPlay";
+import InfoButton from "@/components/InfoButton";
+import TimeTheoremShop from "@/components/tabs/time-studies/tt-shop/TimeTheoremShop";
+import ModernSidebar from "@/components/ui-modes/modern/ModernSidebar";
+import SaveTimer from "@/components/SaveTimer";
+import SpeedrunStatus from "@/components/SpeedrunStatus";
+import BackgroundAnimations from "@/components/BackgroundAnimations";
+import PopupModal from "@/components/modals/PopupModal";
+import ModalProgressBar from "@/components/modals/ModalProgressBar";
+import FadeToBlack from "@/components/tabs/celestial-pelle/FadeToBlack";
+import CreditsContainer from "@/components/tabs/celestial-pelle/CreditsContainer";
+import NewGame from "@/components/tabs/celestial-pelle/NewGame";
+
+export default {
+  name: "GameUiFixedComponents",
+  components: {
+    HowToPlay,
+    InfoButton,
+    TimeTheoremShop,
+    ModernSidebar,
+    SaveTimer,
+    SpeedrunStatus,
+    BackgroundAnimations,
+    PopupModal,
+    ModalProgressBar,
+    FadeToBlack,
+    CreditsContainer,
+    NewGame
+  },
+  computed: {
+    view() {
+      return this.$viewModel;
+    }
+  }
+};
+</script>
+
+
+<template>
+  <div
+    id="ui-fixed"
+    class="c-game-ui--fixed"
+  >
+    <div
+      id="notification-container"
+      class="l-notification-container"
+    />
+    <HowToPlay />
+    <InfoButton />
+    <TimeTheoremShop
+      v-if="view.subtab === 'studies'"
+      class="l-time-studies-tab__tt-shop"
+    />
+    <ModernSidebar v-if="view.newUI" />
+    <SaveTimer />
+    <SpeedrunStatus />
+    <BackgroundAnimations />
+    <PopupModal
+      v-if="view.modal.current"
+      :modal="view.modal.current"
+    />
+    <ModalProgressBar v-if="view.modal.progressBar" />
+    <FadeToBlack />
+    <CreditsContainer />
+    <NewGame />
+  </div>
+</template>
+
+<style scoped>
+.c-game-ui--fixed {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  pointer-events: none;
+
+  z-index: 5;
+  display: flex;
+  justify-content: center;
+  top: 0;
+  left: 0;
+}
+</style>

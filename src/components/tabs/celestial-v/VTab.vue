b46d2be481512861126926ec75973f2cc7ca98d5
Hirame1 <56225774+Hirame1@users.noreply.github.com>
2022-04-26 22:57:22 +0700

    Fix Cel 4 typo

--- src/components/tabs/celestial-v/VTab.vue
+++ src/components/tabs/celestial-v/VTab.vue
@@ -63,7 +63,7 @@ export default {
           {}
         ];
     },
-    vUnlock: () => VUnlock.vAchievementUnlock,
+    vUnlock: () => VUnlocks.vAchievementUnlock,
     runMilestones() {
       return [
         [

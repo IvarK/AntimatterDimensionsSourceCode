eb4b7acca14625800a3b63bffcf609f041a25cd1
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2022-05-19 18:26:19 +0800

    Codefactor likes my import statements now

--- javascripts/core/devtools.js
+++ javascripts/core/devtools.js
@@ -1,7 +1,7 @@
 import { sha512_256 } from "js-sha512";
-import FullScreenAnimationHandler from "./full-screen-animation-handler";
 
 import { DC } from "./constants";
+import FullScreenAnimationHandler from "./full-screen-animation-handler";
 
 /* eslint-disable no-console */
 // Disabling no-console here seems

957c7b124304c176209d2e113432ab782ab7c2d6
L4R5W <66326188+L4R5W@users.noreply.github.com>
2022-05-19 12:14:51 +0200

    keep infinity broken on eternity in doomed (#2607)

--- javascripts/core/eternity.js
+++ javascripts/core/eternity.js
@@ -89,7 +89,7 @@ export function eternity(force, auto, specialConditions = {}) {
   initializeChallengeCompletions();
   initializeResourcesAfterEternity();
 
-  if (!EternityMilestone.keepAutobuyers.isReached) {
+  if (!EternityMilestone.keepAutobuyers.isReached && !(Pelle.isDoomed && PelleUpgrade.keepAutobuyers.canBeApplied)) {
     // Fix infinity because it can only break after big crunch autobuyer interval is maxed
     player.break = false;
   }

db9eb92df5d16ac83b51f9ad687e5e3175bfe485
earth <divine.icbm@gmail.com>
2022-05-21 08:16:01 -0500

    cleanup info modal
    When the credits stuff is merged I may need to figure out a different functionality in order to show the credits as a modal (or however else we want to do it), as right now it's set up to show only links. I can take care of that then, as it'll be easier to think about when we have an actual idea of what we want.

---
+++ src/components/modals/InformationModalButton.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  name: "InformationModalButton",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<template>
+  <span
+    :ach-tooltip="name"
+    class="c-socials--icon__wrapper"
+  >
+    <a
+      :href="link"
+      target="_blank"
+      class="c-socials--icon"
+    ><i :class="icon" /></a>
+  </span>
+</template>
+
+<style scoped>
+.c-socials--icon {
+  color: var(--color-text);
+}
+
+.c-socials--icon__wrapper {
+  transition: all .2s ease-in-out;
+}
+
+.c-socials--icon__wrapper:hover {
+  transform: scale(1.1);
+}
+
+[ach-tooltip]:before {
+  width: 20rem;
+  font-size: 1.5rem;
+}
+</style>

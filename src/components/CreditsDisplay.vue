bca194d62bc1e8d688f1fc1ab3e978347a131795
earth <divine.icbm@gmail.com>
2022-05-21 19:29:30 -0500

    Create common credits display component

---
+++ src/components/CreditsDisplay.vue
@@ -0,0 +1,76 @@
+<script>
+export default {
+  name: "CreditsDisplay",
+  computed: {
+    people() { return GameDatabase.credits.people; },
+    roles() { return GameDatabase.credits.roles; },
+  },
+  methods: {
+    relevantPeople(role) {
+      return this.people
+        .filter(x => (typeof x.roles === "number" ? x.roles === role : x.roles.includes(role)))
+        .sort((a, b) => a.name.localeCompare(b.name));
+    },
+  }
+};
+</script>
+
+<template>
+  <div>
+    <h1 class="c-credits-header">
+      Antimatter Dimensions
+    </h1>
+
+    <div
+      v-for="role in roles.count"
+      :key="role"
+    >
+      <h2 class="c-credits-section">
+        {{ pluralize(roles[role], relevantPeople(role).length) }}
+      </h2>
+      <div :class="{ 'l-credits--bulk': relevantPeople(role).length > 10}">
+        <div
+          v-for="person in relevantPeople(role)"
+          :key="person.name"
+          class="c-credit-entry"
+        >
+          {{ person.name }}
+          <span v-if="person.name2">
+            ({{ person.name2 }})
+          </span>
+        </div>
+      </div>
+    </div>
+
+    <br><br><br><br><br><br><br><br><br>
+    <h1 class="c-credits-header">
+      Thank you so much for playing!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+.c-credits-header {
+  color: yellow;
+}
+
+.c-credits-section {
+  color: white;
+  text-shadow: 1px 1px 2px turquoise;
+  margin-top: 10rem;
+  margin-bottom: 2rem;
+}
+
+.l-credits--bulk {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 76%;
+  position: relative;
+  left: 12%;
+}
+
+.c-credit-entry {
+  font-size: 1.3rem;
+  margin-top: 1rem;
+}
+</style>
--- src/components/modals/CreditsModal.vue
+++ src/components/modals/CreditsModal.vue
@@ -1,21 +1,14 @@
 <script>
+import CreditsDisplay from "@/components/CreditsDisplay";
 import ModalCloseButton from "@/components/modals/ModalCloseButton";
 
 export default {
   name: "CreditsModal",
   components: {
+    CreditsDisplay,
     ModalCloseButton
   },
-  computed: {
-    people() { return GameDatabase.credits.people; },
-    roles() { return GameDatabase.credits.roles; },
-  },
   methods: {
-    relevantPeople(role) {
-      return this.people
-        .filter(x => (typeof x.roles === "number" ? x.roles === role : x.roles.includes(role)))
-        .sort((a, b) => a.name.localeCompare(b.name));
-    },
     close() {
       this.$emit("close");
       Modal.information.show();
@@ -27,38 +20,7 @@ export default {
 <template>
   <div class="l-credits-modal c-credits-modal">
     <ModalCloseButton @click="close" />
-    <div class="c-credits-header">
-      <div class="c-h2p-title">
-        Antimatter Dimensions
-      </div>
-    </div>
-
-    <div
-      v-for="role in roles.count"
-      :key="role"
-    >
-      <h2 class="c-credits-section">
-        {{ pluralize(roles[role], relevantPeople(role).length) }}
-      </h2>
-      <div :class="{ 'l-credits--bulk': relevantPeople(role).length > 10}">
-        <div
-          v-for="person in relevantPeople(role)"
-          :key="person.name"
-          class="c-credit-entry"
-        >
-          {{ person.name }}
-          <span v-if="person.name2">
-            ({{ person.name2 }})
-          </span>
-        </div>
-      </div>
-    </div>
-
-    <br><br><br><br><br><br><br><br><br>
-    <h1 class="c-credits-header">
-      Thank you so much for playing!
-    </h1>
-    <br><br><br><br><br><br><br><br><br>
+    <CreditsDisplay />
   </div>
 </template>
 
@@ -74,29 +36,5 @@ export default {
 .c-credits-modal {
   overflow-y: scroll;
   margin: 0.5rem;
-}
-
-.c-credits-header {
-  color: yellow;
-}
-
-.c-credits-section {
-  color: white;
-  text-shadow: 1px 1px 2px turquoise;
-  margin-top: 10rem;
-  margin-bottom: 2rem;
-}
-
-.l-credits--bulk {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  width: 76%;
-  position: relative;
-  left: 12%;
-}
-
-.c-credit-entry {
-  font-size: 1.3rem;
-  margin-top: 1rem;
 }
 </style>

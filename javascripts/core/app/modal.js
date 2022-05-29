154a8046dc38df94e1ff15a6f14983590e7148c9
WaitingIdly <25394029+WaitingIdly@users.noreply.github.com>
2022-05-28 23:15:53 -0700

    check if modal is open before issuing a close

--- javascripts/core/app/modal.js
+++ javascripts/core/app/modal.js
@@ -78,15 +78,7 @@ export class Modal {
     const modalQueue = ui.view.modal.queue;
     // Add this modal to the front of the queue and sort based on priority to ensure priority is maintained.
     modalQueue.unshift(this);
-
-    // Unfortunately, we can't do it directly because a lot of modal interactions depend on a modal
-    // being shown that shows up at the back, followed by an immediate closing of the current modal.
-    // This will not work if, say, a modal of priority 2 is shown right before a modal of priority 1 is closed.
-    // So we have to wait just a little while.
-    EventHub.ui.on(GAME_EVENT.UPDATE, () => {
-      Modal.sortModalQueue();
-      EventHub.ui.offAll(Modal.sortModalQueue);
-    }, Modal.sortModalQueue);
+    Modal.sortModalQueue();
   }
 
   get isOpen() {

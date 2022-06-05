f28b85023e8642f7c473690436df0824958dcb9b
WaitingIdly <25394029+WaitingIdly@users.noreply.github.com>
2022-06-05 04:01:59 -0700

    split quotes modal up into multiple components

---
+++ src/components/modals/CelestialQuoteBackground.vue
@@ -0,0 +1,141 @@
+<script>
+export default {
+  name: "CelestialQuoteBackground",
+  props: {
+    celestialSymbol: {
+      type: String,
+      required: true
+    },
+    celestial: {
+      type: String,
+      required: true
+    },
+    primary: {
+      type: Boolean,
+      required: true
+    },
+  },
+  computed: {
+    modalClass() {
+      return {
+        "l-modal-celestial-quote": true,
+        [`c-modal-celestial-quote--${this.celestial}`]: true,
+        [`c-modal-celestial-quote-primary--${this.celestial}`]: this.primary,
+      };
+    }
+  },
+};
+</script>
+
+<template>
+  <div :class="modalClass">
+    <span
+      class="c-modal-cestial-quote__symbol"
+      v-html="celestialSymbol"
+    />
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.l-modal-celestial-quote {
+  display: flex;
+  flex-direction: row;
+  width: 30rem;
+  height: 30rem;
+  position: fixed;
+  /* stylelint-disable-next-line unit-allowed-list */
+  top: 50vh;
+  /* stylelint-disable-next-line unit-allowed-list */
+  left: 50vw;
+  z-index: 3;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: var(--var-border-radius, 1rem);
+  transform: translate(-50%, -50%);
+}
+
+.c-modal-cestial-quote__symbol {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  justify-content: center;
+  align-items: center;
+  font-size: 25rem;
+  opacity: 0.2;
+  text-shadow: 0 0 2rem;
+  pointer-events: none;
+}
+
+.c-modal-celestial-quote--teresa {
+  color: var(--color-teresa--base);
+  background-color: black;
+  box-shadow: 0 0 1rem var(--color-teresa--base), 0 0 1rem var(--color-teresa--base) inset;
+}
+
+.c-modal-celestial-quote-primary--teresa {
+  border-color: var(--color-teresa--base);
+}
+
+.c-modal-celestial-quote--effarig {
+  color: var(--color-effarig--base);
+  background-color: black;
+  box-shadow: 0 0 1rem var(--color-effarig--base), 0 0 1rem var(--color-effarig--base) inset;
+}
+
+.c-modal-celestial-quote-primary--effarig {
+  border-color: var(--color-effarig--base);
+}
+
+.c-modal-celestial-quote--enslaved {
+  color: var(--color-enslaved--base);
+  background-color: black;
+  box-shadow: 0 0 1rem var(--color-enslaved--base), 0 0 1rem var(--color-enslaved--base) inset;
+}
+
+.c-modal-celestial-quote-primary--enslaved {
+  border-color: var(--color-enslaved--base);
+}
+
+.c-modal-celestial-quote--v {
+  color: var(--color-v--base);
+  background-color: black;
+  box-shadow: 0 0 1rem var(--color-v--base), 0 0 1rem var(--color-v--base) inset;
+}
+
+.c-modal-celestial-quote-primary--v {
+  border-color: var(--color-v--base);
+}
+
+.c-modal-celestial-quote--ra {
+  color: var(--color-ra--base);
+  background-color: black;
+  box-shadow: 0 0 1rem var(--color-ra--base), 0 0 1rem var(--color-ra--base) inset;
+}
+
+.c-modal-celestial-quote-primary--ra {
+  border-color: var(--color-ra--base);
+}
+
+.c-modal-celestial-quote--laitela {
+  color: var(--color-laitela--accent);
+  background-color: var(--color-laitela--base);
+  box-shadow: 0 0 1rem var(--color-laitela--accent), 0 0 1rem var(--color-laitela--accent) inset;
+}
+
+.c-modal-celestial-quote-primary--laitela {
+  border-color: var(--color-laitela--accent);
+}
+
+.c-modal-celestial-quote--pelle {
+  color: var(--color-pelle--base);
+  background-color: black;
+  box-shadow: 0 0 1rem var(--color-pelle--base), 0 0 1rem var(--color-pelle--base) inset;
+}
+
+.c-modal-celestial-quote-primary--pelle {
+  border-color: var(--color-pelle--base);
+}
+</style>
--- src/components/modals/CelestialQuoteModal.vue
+++ src/components/modals/CelestialQuoteModal.vue
@@ -1,6 +1,11 @@
 <script>
+import CelestialQuoteLine from "./CelestialQuoteLine";
+
 export default {
   name: "CelestialQuoteModal",
+  components: {
+    CelestialQuoteLine
+  },
   props: {
     quote: {
       type: Object,
@@ -10,12 +15,12 @@ export default {
   data() {
     return {
       index: 0,
-      message: "",
-      celestialSymbol: "",
-      celestialName: ""
     };
   },
   computed: {
+    totalLines() {
+      return this.quote.totalLines;
+    },
     currentLine: {
       get() {
         return this.index;
@@ -24,34 +29,12 @@ export default {
         this.index = Math.clamp(x, 0, this.totalLines - 1);
       }
     },
-    line() {
-      return this.quote.line(this.currentLine);
-    },
-    celestial() {
-      return this.line.celestial;
-    },
-    totalLines() {
-      return this.quote.totalLines;
-    },
     isQuoteStart() {
       return this.currentLine === 0;
     },
     isQuoteEnd() {
       return this.currentLine === this.totalLines - 1;
     },
-    prevStyle() {
-      return this.isQuoteStart ? { visibility: "hidden" } : {};
-    },
-    nextStyle() {
-      return this.isQuoteEnd ? { visibility: "hidden" } : {};
-    },
-    modalClass() {
-      return [
-        "l-modal-celestial-quote",
-        "c-modal",
-        `c-modal-celestial-quote--${this.celestial}`
-      ];
-    }
   },
   created() {
     this.$nextTick(() => {
@@ -73,164 +56,30 @@ export default {
       this.index = 0;
       Quote.advanceQueue();
     },
-    update() {
-      const line = this.line;
-      this.message = line.line;
-      this.celestialSymbol = line.celestialSymbol;
-      this.celestialName = line.celestialName;
-    }
   },
 };
 </script>
 
 <template>
   <div class="l-modal-overlay c-modal-overlay">
-    <div :class="modalClass">
-      <span
-        class="c-modal-cestial-quote__symbol"
-        v-html="celestialSymbol"
-      />
-      <span
-        v-if="line.showCelestialName"
-        class="c-modal-celestial-name"
-      >
-        {{ celestialName }}
-      </span>
-      <i
-        :style="prevStyle"
-        class="c-modal-celestial-quote__arrow fas fa-chevron-circle-left"
-        @click="progressIn('left')"
-      />
-      <div class="l-modal-celestial-quote__text">
-        {{ message }}
-      </div>
-      <i
-        :style="nextStyle"
-        class="c-modal-celestial-quote__arrow fas fa-chevron-circle-right"
-        @click="progressIn('right')"
-      />
-      <i
-        v-if="isQuoteEnd"
-        class="c-modal-celestial-quote__end fas fa-check-circle"
-        @click="close"
-      />
-    </div>
+    <CelestialQuoteLine
+      class="c-quote-overlay"
+      :quote="quote"
+      :current-line="currentLine"
+      :left-visible="!isQuoteStart"
+      :right-visible="!isQuoteEnd"
+      :close-visible="isQuoteEnd"
+      primary
+      @close="close"
+      @progress-in="progressIn"
+    />
   </div>
 </template>
 
 <style scoped>
-.l-modal-celestial-quote {
-  display: flex;
-  flex-direction: row;
-  width: 30rem;
-  height: 30rem;
-  position: fixed;
-  /* stylelint-disable-next-line unit-allowed-list */
-  top: 50vh;
-  /* stylelint-disable-next-line unit-allowed-list */
-  left: 50vw;
-  z-index: 3;
-  justify-content: space-between;
-  align-items: center;
-  transform: translate(-50%, -50%);
-}
-
-.c-modal-cestial-quote__symbol {
-  display: flex;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  left: 0;
-  justify-content: center;
-  align-items: center;
-  font-size: 25rem;
-  opacity: 0.2;
-  text-shadow: 0 0 2rem;
-  pointer-events: none;
-}
-
-.c-modal-celestial-quote--teresa {
-  color: var(--color-teresa--base);
-  background-color: black;
-  border-color: var(--color-teresa--base);
-  box-shadow: 0 0 1rem var(--color-teresa--base), 0 0 1rem var(--color-teresa--base) inset;
-}
-
-.c-modal-celestial-quote--effarig {
-  color: var(--color-effarig--base);
-  background-color: black;
-  border-color: var(--color-effarig--base);
-  box-shadow: 0 0 1rem var(--color-effarig--base), 0 0 1rem var(--color-effarig--base) inset;
-}
-
-.c-modal-celestial-quote--enslaved {
-  color: var(--color-enslaved--base);
-  background-color: black;
-  border-color: var(--color-enslaved--base);
-  box-shadow: 0 0 1rem var(--color-enslaved--base), 0 0 1rem var(--color-enslaved--base) inset;
-}
-
-.c-modal-celestial-quote--v {
-  color: var(--color-v--base);
-  background-color: black;
-  border-color: var(--color-v--base);
-  box-shadow: 0 0 1rem var(--color-v--base), 0 0 1rem var(--color-v--base) inset;
-}
-
-.c-modal-celestial-quote--ra {
-  color: var(--color-ra--base);
-  background-color: black;
-  border-color: var(--color-ra--base);
-  box-shadow: 0 0 1rem var(--color-ra--base), 0 0 1rem var(--color-ra--base) inset;
-}
-
-.c-modal-celestial-quote--laitela {
-  color: var(--color-laitela--accent);
-  background-color: var(--color-laitela--base);
-  border-color: var(--color-laitela--accent);
-  box-shadow: 0 0 1rem var(--color-laitela--accent), 0 0 1rem var(--color-laitela--accent) inset;
-}
-
-.c-modal-celestial-quote--pelle {
-  color: var(--color-pelle--base);
-  background-color: black;
-  border-color: var(--color-pelle--base);
-  box-shadow: 0 0 1rem var(--color-pelle--base), 0 0 1rem var(--color-pelle--base) inset;
-}
-
-.c-modal-celestial-name {
-  position: absolute;
-  top: 1rem;
-  right: 0;
-  left: 0;
-  font-weight: bold;
-}
-
-.c-modal-celestial-quote__arrow {
-  font-size: 150%;
-  margin: 0.5rem;
-  cursor: pointer;
-}
-
-.c-modal-celestial-quote__end {
-  position: absolute;
-  bottom: 1.5rem;
-  left: calc(50% - 1rem);
-  font-size: 150%;
-  cursor: pointer;
-}
-
-.l-modal-celestial-quote__text {
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  justify-content: center;
-}
-
-.l-modal-celestial-quote__buttons {
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  justify-content: space-between;
+.c-quote-overlay {
+  font-size: 1.4rem;
+  padding: 1rem;
+  transition-duration: 0.2s;
 }
 </style>

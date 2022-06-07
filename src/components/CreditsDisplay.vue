9b9a4d574a25739448d00f3498fe71252991fc5a
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2022-06-07 21:58:37 +0800

    Make the credits header the standard antimatter colour

--- src/components/CreditsDisplay.vue
+++ src/components/CreditsDisplay.vue
@@ -51,11 +51,35 @@ export default {
 
 <style scoped>
 .c-credits-header {
-  color: #6b237d;
+  color: black;
 }
 
-.s-base--dark .c-credits-header {
-  color: yellow;
+.t-dark .c-credits-header,
+.t-s6 .c-credits-header,
+.t-s10 .c-credits-header {
+  animation: a-credits-header--glow 25s infinite;
+}
+
+@keyframes a-credits-header--glow {
+  0% { color: #2196f3; }
+  33% { color: #673ab7; }
+  66% { color: #00bcd4; }
+  100% { color: #2196f3; }
+}
+
+.t-s11 .c-credits-header {
+  animation: a-credits-header--glow-blob 25s infinite;
+}
+
+@keyframes a-credits-header--glow-blob {
+  0% { color: #fbc21b; }
+  33% { color: #caa32c; }
+  66% { color: #fba11b; }
+  100% { color: #fbc21b; }
+}
+
+.t-dark-metro .c-credits-header {
+  color: #e0e0e0;
 }
 
 .c-credits-section {

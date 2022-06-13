90b6b32d0fbdd3e007e2f9b3a231f3c733b80e62
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2022-06-13 16:41:49 +0800

    Address PR feedback (Save timer covers nothing)

--- src/components/tabs/automator/AutomatorTab.vue
+++ src/components/tabs/automator/AutomatorTab.vue
@@ -89,7 +89,7 @@ export default {
       </span>
       <div class="c-automator-split-pane">
         <SplitPane
-          :min-percent="40"
+          :min-percent="fullScreen ? 33 : 40"
           :default-percent="50"
           split="vertical"
         >

654854b5ede9f5c01f911c9ecc485f023f01798b
Dys <50160441+Dystopia-user181@users.noreply.github.com>
2022-06-19 15:17:35 +0800

    Fix credits modal scrollbar displaying on top of X

--- src/components/modals/CreditsModal.vue
+++ src/components/modals/CreditsModal.vue
@@ -1,34 +1,43 @@
 <script>
 import CreditsDisplay from "@/components/CreditsDisplay";
-import ModalCloseButton from "@/components/modals/ModalCloseButton";
+import ModalWrapper from "@/components/modals/ModalWrapper";
 
 export default {
   name: "CreditsModal",
   components: {
     CreditsDisplay,
-    ModalCloseButton
+    ModalWrapper
   },
 };
 </script>
 
 <template>
-  <div class="l-credits-modal c-credits-modal">
-    <ModalCloseButton @click="emitClose" />
-    <CreditsDisplay />
-  </div>
+  <ModalWrapper class="l-credits-modal c-credits-modal">
+    <template #header>
+      <h1 class="c-game-header__antimatter">
+        Antimatter Dimensions
+      </h1>
+    </template>
+    <div class="c-credits-modal__body">
+      <CreditsDisplay :is-modal="true" />
+    </div>
+  </ModalWrapper>
 </template>
 
 <style scoped>
 .l-credits-modal {
-  display: block;
   /* stylelint-disable-next-line unit-allowed-list */
   width: calc(100vw - 60vh);
-  /* stylelint-disable-next-line unit-allowed-list */
-  height: calc(100vh - 40vh);
+  min-width: 70rem;
 }
 
 .c-credits-modal {
-  overflow-y: scroll;
   margin: 0.5rem;
+}
+
+.c-credits-modal__body {
+  overflow-y: scroll;
+  /* stylelint-disable-next-line unit-allowed-list */
+  height: 60vh;
 }
 </style>

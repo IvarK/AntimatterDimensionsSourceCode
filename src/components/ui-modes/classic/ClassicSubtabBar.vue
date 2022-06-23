8c64f0e3591e040617560344313404e4bd3df176
Omsi <omsi2000@gmail.com>
2022-06-22 18:03:51 -0700

    codefactor fix

--- src/components/ui-modes/classic/ClassicSubtabBar.vue
+++ src/components/ui-modes/classic/ClassicSubtabBar.vue
@@ -34,7 +34,7 @@ export default {
       v-for="(subtab, i) in subtabs"
       :key="i"
       :subtab="subtab"
-      :parentName="tab.name"
+      :parent-name="tab.name"
     />
   </div>
 </template>

7f759c78276e7895d84c012ea52380516c4be650
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2022-07-29 11:16:00 -0500

    Expand text in UI choice modal

--- src/components/modals/UiChoiceModal.vue
+++ src/components/modals/UiChoiceModal.vue
@@ -17,11 +17,22 @@ export default {
 <template>
   <ModalWrapperChoice @confirm="handleYesClick">
     <template #header>
-      Swap to Modern UI
+      Visual Changes
     </template>
-    <div class="c-modal-message__text">
-      We noticed that you've loaded an old save, would you like to swap to the Modern UI?
-      (you can change back to Classic at any time in the Visual Options tab)
+    <div class="c-modal-message__text l-left">
+      We noticed that you've loaded an old save; a few visual changes have been made since older versions of the game:
+      <br>
+      - Many more game events now have animations. If these impact your performance or gameplay, they can be disabled
+      in the Visual Options tab.
+      <br>
+      - New confirmation windows have replaced the default Javascript alert windows. These can be disabled in the
+      Gameplay Options tab.
+      <br>
+      - The game now also has a new sleek layout which was designed with more Modern design practices in mind.
+      <br>
+      <br>
+      You can change between the Classic UI which older versions of the game used and the newer Modern UI in the Visual
+      Options tab at any time. Would you like to swap to the Modern UI now?
     </div>
     <template #cancel-text>
       Remain
@@ -31,3 +42,9 @@ export default {
     </template>
   </ModalWrapperChoice>
 </template>
+
+<style scoped>
+.l-left {
+  text-align: left;
+}
+</style>

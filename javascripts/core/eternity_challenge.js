25a21156c55c81ba0a33af0fe834d3d376d47eb1
Dan Simon <des480@nyu.edu>
2022-08-26 13:03:36 -0400

    Change wording

--- javascripts/core/eternity_challenge.js
+++ javascripts/core/eternity_challenge.js
@@ -250,7 +250,7 @@ export class EternityChallengeState extends GameMechanicState {
       `spending more than ${quantify("in-game second", restriction, 0, 1)} in it`;
     }
     Modal.message.show(`${reason(this.config.restriction(this.completions))}; ` +
-    `you have now exited ${auto ? "that challenge" : "it"}.`,
+    `which has caused you to exit it.`;
     { closeEvent: GAME_EVENT.REALITY_RESET_AFTER }, 1);
     EventHub.dispatch(GAME_EVENT.CHALLENGE_FAILED);
   }

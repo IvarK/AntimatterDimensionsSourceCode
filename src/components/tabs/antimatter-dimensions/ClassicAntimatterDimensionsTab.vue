02dddad206141c476f5cd847f5a68e303aad80bf
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2022-09-09 16:50:29 -0500

    Unify header components in Classic/Modern UI

--- src/components/tabs/antimatter-dimensions/ClassicAntimatterDimensionsTab.vue
+++ src/components/tabs/antimatter-dimensions/ClassicAntimatterDimensionsTab.vue
@@ -18,11 +18,7 @@ export default {
   },
   data() {
     return {
-      isInMatterChallenge: false,
-      matter: new Decimal(0),
       hasDimensionBoosts: false,
-      isChallengePowerVisible: false,
-      challengePower: "",
       isQuickResetAvailable: false,
       isSacrificeUnlocked: false,
       buy10Mult: new Decimal(0),
@@ -32,27 +28,7 @@ export default {
   },
   methods: {
     update() {
-      this.isInMatterChallenge = Player.isInMatterChallenge;
-      if (this.isInMatterChallenge) {
-        this.matter.copyFrom(Currency.matter);
-      }
       this.hasDimensionBoosts = player.dimensionBoosts > 0;
-      const isC2Running = NormalChallenge(2).isRunning;
-      const isC3Running = NormalChallenge(3).isRunning;
-      const isIC6Running = InfinityChallenge(6).isRunning;
-      const isIC8Running = InfinityChallenge(8).isRunning;
-      const isChallengePowerVisible = isC2Running || isC3Running || isIC6Running || isIC8Running;
-      this.isChallengePowerVisible = isChallengePowerVisible;
-      if (isChallengePowerVisible) {
-        const powerArray = [];
-        if (isC2Running) powerArray.push(`Production: ${formatPercents(player.chall2Pow, 2, 2)}`);
-        if (isC3Running) powerArray.push(`First dimension: ${formatX(player.chall3Pow, 3, 4)}`);
-        if (isIC6Running) powerArray.push(`Matter: /
-          ${format(new Decimal(1).timesEffectOf(InfinityChallenge(6)), 2, 2)}`);
-        if (isIC8Running) powerArray.push(`Production: /
-          ${format(new Decimal(1).timesEffectOf(InfinityChallenge(8)).reciprocal(), 2, 2)}`);
-        this.challengePower = powerArray.join(", ");
-      }
       this.isQuickResetAvailable = Player.isInAntimatterChallenge && Player.antimatterChallenge.isQuickResettable;
       this.isSacrificeUnlocked = Sacrifice.isVisible;
       this.buy10Mult.copyFrom(AntimatterDimensions.buyTenMultiplier);
@@ -73,8 +49,6 @@ export default {
   <div class="l-old-ui-antimatter-dim-tab">
     <span>{{ multiplierText }}</span>
     <AntimatterDimensionsTabHeader />
-    <span v-if="isInMatterChallenge">There is {{ format(matter, 2, 1) }} matter.</span>
-    <span v-if="isChallengePowerVisible">{{ challengePower }}</span>
     <div class="l-dimensions-container">
       <AntimatterDimensionRow
         v-for="tier in 8"
--- src/components/ui-modes/modern/ModernUi.vue
+++ src/components/ui-modes/modern/ModernUi.vue
@@ -1,42 +1,24 @@
 <script>
-import HeaderBigCrunchButton from "../HeaderBigCrunchButton";
 import HeaderBlackHole from "../HeaderBlackHole";
 import HeaderChallengeDisplay from "../HeaderChallengeDisplay";
-import HeaderEternityButton from "../HeaderEternityButton";
-import HeaderUnlockInfinityDimButton from "../HeaderUnlockInfinityDimButton";
+import HeaderChallengeEffects from "../HeaderChallengeEffects";
+import HeaderPrestigeGroup from "../HeaderPrestigeGroup";
 import NewsTicker from "../NewsTicker";
 
-import GameSpeedDisplay from "@/components/GameSpeedDisplay";
 
 export default {
   name: "ModernUi",
   components: {
     HeaderChallengeDisplay,
-    HeaderEternityButton,
-    HeaderBigCrunchButton,
-    HeaderUnlockInfinityDimButton,
+    HeaderChallengeEffects,
     NewsTicker,
     HeaderBlackHole,
-    GameSpeedDisplay
+    HeaderPrestigeGroup
   },
   data() {
     return {
       bigCrunch: false,
       smallCrunch: false,
-      realities: 0,
-      antimatter: new Decimal(0),
-      antimatterPerSec: new Decimal(0),
-      isInMatterChallenge: false,
-      matter: new Decimal(0),
-      isInEffarig: false,
-      effarigMultNerfText: "",
-      effarigTickNerfText: "",
-      isInLaitela: false,
-      laitelaTimer: 0,
-      laitelaEntropy: "",
-      isChallengePowerVisible: false,
-      challengePower: "",
-      isInFailableEC: false,
     };
   },
   computed: {
@@ -49,57 +31,10 @@ export default {
   },
   methods: {
     update() {
-      this.antimatter.copyFrom(Currency.antimatter);
-      this.antimatterPerSec.copyFrom(Currency.antimatter.productionPerSecond);
-      this.realities = Currency.realities.value;
-
-      this.currentEternityChallenge = EternityChallenge.current;
-      this.isInFailableEC = this.currentEternityChallenge && [4, 12].includes(this.currentEternityChallenge.id);
-
-      this.updateChallengePower();
-
-      this.isInMatterChallenge = Player.isInMatterChallenge;
-      if (this.isInMatterChallenge) {
-        this.matter.copyFrom(Currency.matter);
-      }
-      this.isInEffarig = Effarig.isRunning;
-      if (this.isInEffarig) {
-        this.effarigMultNerfText = `${formatPow(0.25 + 0.25 * Effarig.nerfFactor(Currency.infinityPower.value), 0, 5)}`;
-        this.effarigTickNerfText = `${formatPow(0.7 + 0.1 * Effarig.nerfFactor(Currency.timeShards.value), 0, 5)}`;
-      }
-      this.isInLaitela = Laitela.isRunning;
-      if (this.isInLaitela) {
-        if (player.celestials.laitela.entropy > 0) {
-          this.laitelaEntropy = `${formatPercents(player.celestials.laitela.entropy, 2, 2)}`;
-          this.laitelaTimer = Time.thisRealityRealTime.toStringShort();
-        } else {
-          this.laitelaEntropy = `${formatPercents(1, 2, 2)}`;
-          this.laitelaTimer = TimeSpan.fromSeconds(player.celestials.laitela.thisCompletion).toStringShort();
-        }
-      }
-
       const crunchButtonVisible = !player.break && Player.canCrunch;
       const reachedInfinityInMinute = Time.bestInfinityRealTime.totalMinutes <= 1;
       this.bigCrunch = crunchButtonVisible && !reachedInfinityInMinute;
       this.smallCrunch = crunchButtonVisible && reachedInfinityInMinute;
-    },
-    updateChallengePower() {
-      const isC2Running = NormalChallenge(2).isRunning;
-      const isC3Running = NormalChallenge(3).isRunning;
-      const isIC6Running = InfinityChallenge(6).isRunning;
-      const isIC8Running = InfinityChallenge(8).isRunning;
-      const isChallengePowerVisible = isC2Running || isC3Running || isIC6Running || isIC8Running;
-      this.isChallengePowerVisible = isChallengePowerVisible;
-      if (isChallengePowerVisible) {
-        const powerArray = [];
-        if (isC2Running) powerArray.push(`Production: ${formatPercents(player.chall2Pow, 2, 2)}`);
-        if (isC3Running) powerArray.push(`First dimension: ${formatX(player.chall3Pow, 3, 4)}`);
-        if (isIC6Running) powerArray.push(`Matter: /
-          ${format(new Decimal(1).timesEffectOf(InfinityChallenge(6)), 2, 2)}`);
-        if (isIC8Running) powerArray.push(`Production: /
-          ${format(new Decimal(1).timesEffectOf(InfinityChallenge(8)).reciprocal(), 2, 2)}`);
-        this.challengePower = powerArray.join(", ");
-      }
     },
     handleClick() {
       if (PlayerProgress.infinityUnlocked()) bigCrunchResetRequest();
@@ -137,35 +72,10 @@ export default {
         v-else
         class="tab-container"
       >
-        <div class="l-reset-buttons-container">
-          <HeaderEternityButton />
-          <HeaderUnlockInfinityDimButton />
-          <HeaderBigCrunchButton />
-        </div>
-        <div class="l-game-header__antimatter-container">
-          <p>You have <span class="c-game-header__antimatter">{{ format(antimatter, 2, 1) }}</span> antimatter.</p>
-          <div>You are getting {{ format(antimatterPerSec, 2, 0) }} antimatter per second.</div>
-          <br>
-          <GameSpeedDisplay :is-standalone="true" />
-        </div>
+        <HeaderPrestigeGroup />
         <div class="information-header">
           <HeaderChallengeDisplay />
-          <div v-if="isInEffarig">
-            Game speed and multipliers are Dilated {{ effarigMultNerfText }}
-            <br>
-            Tickspeed is Dilated {{ effarigTickNerfText }}
-          </div>
-          <div v-if="isInLaitela">
-            Entropy: {{ laitelaEntropy }} ({{ laitelaTimer }})
-          </div>
-          <br>
-          <span v-if="isInMatterChallenge">
-            There is {{ format(matter, 2, 1) }} matter.
-          </span>
-          <span v-if="isChallengePowerVisible">
-            {{ challengePower }}
-          </span>
-          <br v-if="isInMatterChallenge || isChallengePowerVisible">
+          <HeaderChallengeEffects />
           <HeaderBlackHole />
         </div>
         <button

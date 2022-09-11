d60e22b94f2102878d3139d9793897461b1649cd
earth <divine.icbm@gmail.com>
2022-09-11 09:38:11 -0500

    fix codefactor issue

--- javascripts/core/dilation.js
+++ javascripts/core/dilation.js
@@ -56,7 +56,7 @@ const DIL_UPG_NAMES = [
 ];
 
 export function buyDilationUpgrade(id, bulk = 1) {
-  if (GameEnd.creditsClosed) return;
+  if (GameEnd.creditsClosed) return false;
   // Upgrades 1-3 are rebuyable, and can be automatically bought in bulk with a perk shop upgrade
   const upgrade = DilationUpgrade[DIL_UPG_NAMES[id]];
   if (id > 3 && id < 11) {

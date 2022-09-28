1590534b8af878fdc68c6c88ed6efd5170d27eba
Dan Simon <des480@nyu.edu>
2022-09-28 12:04:03 -0400

    Change back to 5%

--- javascripts/core/secret-formula/achievements/normal-achievements.js
+++ javascripts/core/secret-formula/achievements/normal-achievements.js
@@ -923,9 +923,9 @@ GameDatabase.achievements.normal = [
     checkRequirement: () => Currency.infinitiesBanked.gt(DC.D2E9),
     checkEvent: [GAME_EVENT.ETERNITY_RESET_AFTER, GAME_EVENT.SAVE_CONVERTED_FROM_PREVIOUS_VERSION],
     get reward() {
-      return `After Eternity you permanently keep ${formatPercents(0.1)} of your Infinities as Banked Infinities.`;
+      return `After Eternity you permanently keep ${formatPercents(0.05)} of your Infinities as Banked Infinities.`;
     },
-    effect: () => Currency.infinities.value.times(0.1).floor()
+    effect: () => Currency.infinities.value.times(0.05).floor()
   },
   {
     id: 132,

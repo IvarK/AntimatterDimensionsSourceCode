899997f641c58369b6974a5c15d1d653a7f179fa
dan-simon <des480@nyu.edu>
2022-09-30 14:27:25 -0400

    Change no ethical consumption from 5 billion to 2 billion, change banking from 5% to 10%, fixes #3053 (#3056)
    * Change no ethical consuption from 5 billion to 2 billion, change banking from 5% to 10%
    
    * Change back to 5%

--- javascripts/core/secret-formula/achievements/normal-achievements.js
+++ javascripts/core/secret-formula/achievements/normal-achievements.js
@@ -919,9 +919,9 @@ GameDatabase.achievements.normal = [
   {
     id: 131,
     name: "No ethical consumption",
-    get description() { return `Get ${format(DC.D5E9)} Banked Infinities.`; },
-    checkRequirement: () => Currency.infinitiesBanked.gt(DC.D5E9),
-    checkEvent: GAME_EVENT.ETERNITY_RESET_AFTER,
+    get description() { return `Get ${format(DC.D2E9)} Banked Infinities.`; },
+    checkRequirement: () => Currency.infinitiesBanked.gt(DC.D2E9),
+    checkEvent: [GAME_EVENT.ETERNITY_RESET_AFTER, GAME_EVENT.SAVE_CONVERTED_FROM_PREVIOUS_VERSION],
     get reward() {
       return `After Eternity you permanently keep ${formatPercents(0.05)} of your Infinities as Banked Infinities.`;
     },

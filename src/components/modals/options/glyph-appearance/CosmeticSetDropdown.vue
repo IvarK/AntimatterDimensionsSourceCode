8234051c6162918ec7db80d079929a316b24d32a
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2022-11-26 01:46:43 -0600

    Add cosmetic set selection modal

---
+++ src/components/modals/options/glyph-appearance/CosmeticSetDropdown.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: "CosmeticSetDropdown",
+  computed: {
+    lockedSets() {
+      return GlyphAppearanceHandler.lockedSets.map(key => GameDatabase.reality.glyphCosmeticSets[key]);
+    }
+  },
+  methods: {
+    select(set) {
+      GlyphAppearanceHandler.setInModal = set;
+      this.$parent.openRequest = false;
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="l-select-theme">
+    <div class="l-select-theme__inner">
+      <div
+        v-for="set in lockedSets"
+        :key="set.id"
+        class="o-primary-btn l-select-theme__item c-select-theme__item"
+        @click="select(set)"
+      >
+        {{ set.name }}
+      </div>
+    </div>
+  </div>
+</template>

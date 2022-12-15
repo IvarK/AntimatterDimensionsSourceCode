fd122887757cacf00e5a952cbaca91b1cfb979ce
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2022-12-15 13:42:53 -0600

    Add warnings for cosmetics/speedrun on import

--- javascripts/core/app/player-progress.js
+++ javascripts/core/app/player-progress.js
@@ -20,6 +20,10 @@ export class PlayerProgress {
     return this._player.realities > 0;
   }
 
+  get hasFullCompletion() {
+    return this._player.records?.fullGameCompletions > 0;
+  }
+
   static get current() {
     return new PlayerProgress(player);
   }

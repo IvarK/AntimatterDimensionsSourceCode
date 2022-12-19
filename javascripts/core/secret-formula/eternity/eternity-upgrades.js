e11bc4c85dd226fd73aa9f0b6eefdfe11369d2cf
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2022-12-18 21:15:58 -0600

    Fix EU2 for older migrated saves

--- javascripts/core/secret-formula/eternity/eternity-upgrades.js
+++ javascripts/core/secret-formula/eternity/eternity-upgrades.js
@@ -32,7 +32,9 @@ GameDatabase.eternity.upgrades = {
     id: 3,
     cost: 5e4,
     description: "Infinity Dimensions multiplier based on sum of Infinity Challenge times",
-    effect: () => DC.D2.pow(30 / Time.infinityChallengeSum.totalSeconds),
+    // The cap limits this at a lower value, but we also need an explicit cap here because very old versions have
+    // allowed EC12 to make all the challenge records sum to zero (causing a division by zero here)
+    effect: () => DC.D2.pow(30 / Math.clampMin(Time.infinityChallengeSum.totalSeconds, 0.1)),
     cap: DC.D2P30D0_61,
     formatEffect: value => formatX(value, 2, 1)
   },

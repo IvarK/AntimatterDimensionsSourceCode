59c5436da2c7283f9c71dada6a0ce50fb9d41114
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2023-02-27 07:52:15 -0600

    Reduce savefile size impact for speedrun data

--- src/components/tabs/speedrun-milestones/SpeedrunMilestonesTab.vue
+++ src/components/tabs/speedrun-milestones/SpeedrunMilestonesTab.vue
@@ -10,10 +10,14 @@ export default {
   },
   data() {
     return {
-      milestones: [],
+      milestoneTimes: [],
+      maxMilestone: 1,
       startTimeStr: "",
       displayAll: false,
     };
+  },
+  computed: {
+    milestones: () => GameDatabase.speedrunMilestones,
   },
   watch: {
     displayAll(newValue) {
@@ -22,25 +26,12 @@ export default {
   },
   methods: {
     update() {
-      this.displayAll = player.speedrun.displayAllMilestones;
-      const db = GameDatabase.speedrunMilestones;
-      const idList = db.map(m => m.id);
-      this.milestones = [];
-      for (const completed of idList) {
-        const milestone = db.find(m => m.id === completed);
-        const time = player.speedrun.records[milestone.key];
-        this.milestones.push({
-          db: db.find(m => m.id === completed),
-          time,
-        });
-      }
-      // It's possible to complete milestones out of order,
-      // so we have to check which was the last completed and show everything up to that + the next one.
-      const maxMilestone = this.milestones.map(i => Boolean(i.time)).lastIndexOf(true);
-      this.milestones.forEach((m, i) => m.display = this.displayAll || (i <= maxMilestone + 1));
+      this.milestoneTimes = [...player.speedrun.records];
+      this.maxMilestone = this.milestoneTimes.map(i => Boolean(i)).lastIndexOf(true) + 1;
       this.startTimeStr = player.speedrun.startDate === 0
         ? "Speedrun not started yet."
         : `Speedrun started at ${Time.toDateTimeString(player.speedrun.startDate)}`;
+      this.displayAll = player.speedrun.displayAllMilestones;
     },
   },
 };
@@ -60,9 +51,10 @@ export default {
       <SpeedrunMilestoneSingle
         v-for="milestone in milestones"
         :key="milestone.id"
-        :milestone="milestone.db"
-        :display="milestone.display"
-        :time="milestone.time"
+        :milestone="milestone"
+        :index="milestone.id"
+        :display="displayAll || milestone.id <= maxMilestone"
+        :time="milestoneTimes[milestone.id]"
       />
     </div>
   </div>

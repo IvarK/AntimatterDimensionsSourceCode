5192282be542a9f00c76af423477d4ecfddd313c
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2023-04-20 22:31:31 -0500

    Make errored block conversion less aggressive (Fixes #3317)

--- src/components/tabs/automator/AutomatorEditor.vue
+++ src/components/tabs/automator/AutomatorEditor.vue
@@ -64,8 +64,15 @@ export default {
       // This may happen if the player has errored textmato scripts and switches to them while in blockmato mode
       if (BlockAutomator.hasUnparsableCommands(this.currentScript) &&
         player.reality.automator.type === AUTOMATOR_TYPE.BLOCK) {
-        Modal.message.show(`Some script commands were unrecognizable - defaulting to text editor.`);
+        Modal.message.show(`Some incomplete blocks were unrecognizable - defaulting to text editor.`);
+
+        // AutomatorBackend.changeModes initializes the new editor and savefile state from BlockAutomator.lines, which
+        // will be empty if this is running upon game load - this ends up wiping the entire script. So we instead set
+        // the new script content external to that method call to keep most of the script intact
+        const erroredScript = AutomatorData.currentScriptText();
         AutomatorBackend.changeModes(this.currentScriptID);
+        player.reality.automator.scripts[this.currentScriptID].content = erroredScript;
+        this.automatorType = AUTOMATOR_TYPE.TEXT;
       }
       this.$nextTick(() => BlockAutomator.updateEditor(this.currentScript));
     },

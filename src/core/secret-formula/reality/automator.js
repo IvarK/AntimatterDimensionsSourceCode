91959e4037b115c09df157ba0f709c8a22f08cd1
SpectralFlame <4493131+cyip92@users.noreply.github.com>
2023-05-02 21:47:17 -0500

    Address PR comments (automator-fixes-and-features)

--- src/core/secret-formula/reality/automator.js
+++ src/core/secret-formula/reality/automator.js
@@ -480,7 +480,7 @@ GameDatabase.reality.automator = {
         <blockquote>commands</blockquote>
       }`,
       description: `Defines an inner block of the script where commands are repeated; the comparison is checked at the
-        start and every time the loop repeats. If the condition is true when the WHILE statement is first reached, the
+        start and every time the loop repeats. If the condition is false when the WHILE statement is first reached, the
         inner block of commands will be skipped entirely.`,
       examples: [
         `while ep < 1e500`,

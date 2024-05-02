462cc4785256891529703f9785a0908c6897acbb
kajfik <kajf.jakub@gmail.com>
2024-05-02 15:17:00 +0200

    Made giraffes more expensive, cheaper pp for everyone (#3429)
    * Made giraffes more expensive, cheaper pp for everyone
    
    * Fix perk shop position before fully poured
    
    ---------
    
    Co-authored-by: Hirame1 <56225774+Hirame1@users.noreply.github.com>

--- src/components/tabs/celestial-teresa/TeresaTab.vue
+++ src/components/tabs/celestial-teresa/TeresaTab.vue
@@ -251,10 +251,6 @@ export default {
         </div>
       </div>
       <div
-        v-if="pouredAmount < pouredAmountCap"
-        class="l-rm-container-labels l-teresa-mechanic-container"
-      />
-      <div
         v-if="hasPerkShop"
         class="c-teresa-shop"
       >
@@ -268,6 +264,10 @@ export default {
         />
         You can now modify the appearance of your Glyphs to look like Music Glyphs.
       </div>
+      <div
+        v-else
+        class="l-rm-container-labels l-teresa-mechanic-container"
+      />
     </div>
   </div>
 </template>

e0f273ab6a18679aabf344a45e70620e9a226cec
Supersonic-Seven <benjaminrockholtz@TandemEDUGA.onmicrosoft.com>
2024-09-06 17:39:00 -0400

    Update pelle.js

--- src/core/celestials/pelle/pelle.js
+++ src/core/celestials/pelle/pelle.js
@@ -99,7 +99,6 @@ export const Pelle = {
     // for the group toggle is hidden until they're all re-upgraded to the max again.
     player.auto.antimatterDims.isActive = true;
 
-    player.buyUntil10 = true;
     player.records.realTimeDoomed = 0;
     for (const res of AlchemyResources.all) res.amount = 0;
     AutomatorBackend.stop();

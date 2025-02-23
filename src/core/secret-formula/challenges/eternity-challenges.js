4e8b997f38dbec42b32b446006c0266fa3d3d5ab
Supersonic Seven <b.rockholz@icloud.com>
2025-02-22 21:15:23 -0500

    Update eternity-challenges.js
    EC6 finish pls

--- src/core/secret-formula/challenges/eternity-challenges.js
+++ src/core/secret-formula/challenges/eternity-challenges.js
@@ -86,7 +86,7 @@ export const eternityChallenges = [
     },
     goal: DC.E800,
     pelleGoal: DC.E1500,
-    goalIncrease: DC.E230,
+    goalIncrease: DC.E220,
     reward: {
       description: "Further reduce Antimatter Dimension cost multiplier growth",
       effect: completions => completions * 0.2,
